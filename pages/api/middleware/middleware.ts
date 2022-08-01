0b4dc7325ef387a88871e87984fe03034b46f224
Jongwoo Park <jongwoo.park@code.berlin>
2022-08-01 17:18:20 +0200

    2022-08-01 12:35:30

--- pages/api/middleware/middleware.ts
+++ pages/api/middleware/middleware.ts
@@ -1,6 +1,18 @@
 export default function middleware(req) {
   console.log(req.geo.city)
   console.log(req.headers.get("x-vercel-up-city"))
+  console.log(req.headers.get("x-vercel-up-country"))
+  console.log(req.headers.get("x-vercel-up-timezone"))
+  console.log(req.headers.get("x-vercel-up-ip"))
+  console.log(req.headers.get("x-vercel-up-latitude"))
+  console.log(req.headers.get("x-vercel-up-longitude"))
+  console.log(req.headers.get("x-vercel-up-tor"))
+  console.log(req.headers.get("x-vercel-up-fingerprint"))
+  console.log(req.headers.get("x-vercel-up-user-agent"))
+  console.log(req.headers.get("x-vercel-up-platform"))
+  console.log(req.headers.get("x-vercel-up-language"))
+  console.log(req.headers.get("x-vercel-up-path-name"))
+
   req.setHeader("x-vercel-up-city", req.geo.city)
   req.setHeader("x-vercel-up-country", req.geo.country)
   req.setHeader("x-vercel-up-timezone", req.geo.timezone)
