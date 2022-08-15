cedf61ad2da08415e8883213079f2c7680269045
Jongwoo Park <jongwoo.park@code.berlin>
2022-08-15 14:25:24 +0200

    umami tracking script added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,32 @@ function SafeHydrate({ children }) {
     </div>
   )
 }
+
+// export function reportWebVitals(metric) {
+//   switch (metric.name) {
+//     case "FCP":
+//       // handle FCP results
+//       break
+//     case "LCP":
+//       // handle LCP results
+//       break
+//     case "CLS":
+//       // handle CLS results
+//       break
+//     case "FID":
+//       // handle FID results
+//       break
+//     case "TTFB":
+//       // handle TTFB results
+//       break
+//     case "INP":
+//       // handle INP results (note: INP is still an experimental metric)
+//       break
+//     default:
+//       break
+//   }
+// }
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
@@ -22,6 +48,12 @@ function MyApp({ Component, pageProps }: AppProps) {
             name="viewport"
             content="initial-scale=1.0, width=device-width"
           />
+          <script
+            async
+            defer
+            data-website-id="149cf6e0-3ac8-4781-b4c3-d10a8bf3437e"
+            src="https://dcl-metrics-telemetry.herokuapp.com/umami.js"
+          ></script>
         </Head>
         <Component {...pageProps} />
       </SafeHydrate>
