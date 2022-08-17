b0e1f9d6a22d14c7f7b9c807e66d045c136e769b
Jongwoo Park <jongwoo.park@code.berlin>
2022-08-17 13:26:13 +0200

    removed comment

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,13 +4,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_TOKEN) {
     return res.status(401).json({ message: "Invalid token" })
   }
 
   try {
-    // revalidate index(global) page
     await res.revalidate("/")
     return res.json({ revalidated: true })
   } catch (err) {
