5336f394f8e55f23f3b80c857f84b1f2358f68b1
Jongwoo Park <jongwoo.park@code.berlin>
2022-08-18 17:14:16 +0200

    added sentry env var on heroku pipeline & removed unused code

--- src/components/local/stats/Explorers.tsx
+++ src/components/local/stats/Explorers.tsx
@@ -27,13 +27,11 @@ const Explorers = ({ res, isLoading }) => {
 
   const data = Object.entries(res)
 
-  // table pagination
   const [page, setPage] = useState(1)
   const [rowsPerPage, setRowsPerPage] = useState(10)
   const pages = Math.ceil(data.length / rowsPerPage)
   const dataPaginated = data.slice((page - 1) * rowsPerPage, page * rowsPerPage)
 
-  // create a table with data
   const TableComponent = () => {
     const { colorMode } = useColorMode()
     return (
