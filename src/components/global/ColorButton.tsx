fae3413b6d72b5caecb7605f49d4e95f485e6740
Jongwoo Park <jongwoo.park@code.berlin>
2022-08-22 16:07:04 +0200

    added tooltip on theme change btn too

--- src/components/global/ColorButton.tsx
+++ src/components/global/ColorButton.tsx
@@ -1,12 +1,26 @@
-import { Button, useColorMode } from "@chakra-ui/react"
+import {
+  Button,
+  Tooltip,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react"
 import { FiMoon, FiSun } from "react-icons/fi"
 const ColorButton = () => {
   const { colorMode, toggleColorMode } = useColorMode()
   return (
     <>
-      <Button size="lg" variant="link" onClick={toggleColorMode}>
-        {colorMode === "light" ? <FiMoon /> : <FiSun />}
-      </Button>
+      <Tooltip
+        label="theme change"
+        placement="auto"
+        fontSize="sm"
+        bg={useColorModeValue("gray.300", "gray.700")}
+        color={useColorModeValue("black", "white")}
+        borderRadius="md"
+      >
+        <Button size="lg" variant="link" onClick={toggleColorMode}>
+          {colorMode === "light" ? <FiMoon /> : <FiSun />}
+        </Button>
+      </Tooltip>
     </>
   )
 }
