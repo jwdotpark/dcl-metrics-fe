760890a57c756611f3e817af9b834882439a6e19
Jongwoo Park <jongwoo.park@code.berlin>
2022-08-23 14:17:38 +0200

    sentry setup changed

--- next.config.js
+++ next.config.js
@@ -9,11 +9,8 @@ const nextConfig = {
   },
 }
 
-const sampleRate = process.env.NEXT_PUBLIC_TRACE_SAMPLE_RATE
 const sentryWebpackPluginOptions = {
   silent: true,
-  traceSampleRate: sampleRate,
-  // FIXME possibly dcl-metrics.com only later
   enabled: process.env.NEXT_PUBLIC_ENV === "prod",
 }
 
