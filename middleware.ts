079c50e6e709f9e0cadecc75063b34941ed3c89a
Jongwoo Park <jongwoo.park@code.berlin>
2022-08-26 18:20:49 +0200

    date

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,3 @@
 import sslRedirect from "next-ssl-redirect-middleware"
 
 export default sslRedirect({})
-<<<<<<< HEAD
-
-// import { NextFetchEvent, NextRequest, NextResponse } from "next/server"
-
-// type Environment = "production" | "development" | "other"
-// export function middleware(req: NextRequest, ev: NextFetchEvent) {
-//   const currentEnv = process.env.NODE_ENV as Environment
-
-//   if (
-//     currentEnv === "production" &&
-//     req.headers.get("x-forwarded-proto") !== "https"
-//   ) {
-//     return NextResponse.redirect(
-//       `https://${req.nextUrl.hostname}${req.nextUrl.pathname}`,
-//       301
-//     )
-//   }
-//   return NextResponse.next()
-// }
-=======
->>>>>>> preview
