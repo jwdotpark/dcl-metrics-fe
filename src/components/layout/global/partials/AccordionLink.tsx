5d5b91c8931258c9ff329e396cb9efdd86d96d82
Jongwoo Park <jongwoo.park@code.berlin>
2022-10-07 12:06:08 +0200

    accordion link fixed

--- src/components/layout/global/partials/AccordionLink.tsx
+++ src/components/layout/global/partials/AccordionLink.tsx
@@ -4,16 +4,18 @@ import { useRouter } from "next/router"
 const AccordionLink = ({ name }) => {
   const router = useRouter()
   return (
-    <Center w="100%">
+    <Center w="100%" py="4">
       <Button
         bg={useColorModeValue("#6272a4", "#282a36")}
+        border="2px solid"
+        borderColor={useColorModeValue("#282a3625", "#6272a425")}
         borderRadius="xl"
         shadow="md"
         _hover={{
           bg: useColorModeValue("gray.600", "gray.700"),
         }}
         onClick={() => router.push(`/${name}`)}
-        size="sm"
+        size="md"
         variant="ghost"
       >
         <Text color={useColorModeValue("#fff", "gray.200")} fontSize="sm">
