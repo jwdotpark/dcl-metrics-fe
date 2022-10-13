90a9b1c66de91f65f72a8b6182eb53b5853b8470
Jongwoo Park <jongwoo.park@code.berlin>
2022-10-13 16:41:20 +0200

    status page added

--- pages/status.tsx
+++ pages/status.tsx
@@ -1,26 +1,11 @@
 // @ts-nocheck
-import {
-  Flex,
-  Box,
-  useBreakpointValue,
-  Grid,
-  useColorModeValue,
-} from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 import Layout from "../src/components/layout/layout"
-import staticGlobal from "../public/data/cached_global_response.json"
 import staticPeerStatus from "../public/data/peerstats.json"
-import { useAtom } from "jotai"
-import { DataAtom, LoadingStateAtom } from "../src/lib/hooks/atoms"
 import StatusBox from "../src/components/local/status/StatusBox"
 
 const Status = () => {
-  const gridColumn = useBreakpointValue({ md: 1, lg: 1, xl: 2 })
-  // const [data] = useAtom(DataAtom)
-  // const [isDataLoading] = useAtom(LoadingStateAtom)
-  // const result = data.length !== 0 ? data : staticGlobal
   const data = staticPeerStatus
-  console.log("data", data)
-
   return (
     <Layout>
       <Box>
--- src/components/local/status/StatusBox.tsx
+++ src/components/local/status/StatusBox.tsx
@@ -1,4 +1,23 @@
-import { SimpleGrid, Box, useColorModeValue } from "@chakra-ui/react"
+/* eslint-disable react-hooks/rules-of-hooks */
+import {
+  Text,
+  SimpleGrid,
+  Box,
+  useColorModeValue,
+  Flex,
+  Spacer,
+  Tooltip,
+  TableContainer,
+  Table,
+  TableCaption,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react"
+import CountUp from "react-countup"
 import GridBox from "../GridBox"
 import ErrorBox from "../stats/error/ErrorBox"
 
@@ -10,7 +29,7 @@ const StatusBox = ({ data }) => {
   }
 
   const successRate = (success, total) => {
-    return (success / total) * 100
+    return Math.floor((success / total) * 100)
   }
 
   const successArr = Object.entries(data).map((item) => {
@@ -20,22 +39,89 @@ const StatusBox = ({ data }) => {
 
   const isError = successArr.some((item) => item < 75)
 
+  const successColor = (successRate) => {
+    if (successRate >= 80) {
+      return "#50fa7b"
+    } else if (successRate < 80 && successRate >= 60) {
+      return "#ffb86c"
+    } else {
+      return "#ff5555"
+    }
+  }
+
   return (
     <Box w="100%">
       <ErrorBox isError={isError} />
-      <SimpleGrid gap={4} columns={[1, 2]}>
+      <SimpleGrid gap={4} columns={[1, 2, 2, 2, 4]}>
         {data.map((item, i) => (
           <GridBox box={box} key={i} w="100%" border="1px solid red">
             <Box m="4">
-              <Box>
-                Success rate:{" "}
-                {successRate(item.success_count, item.total_count)}
+              <Flex>
+                <Box mx="4">
+                  <Text
+                    as="kbd"
+                    fontSize={["sm", "md", "lg", "xl"]}
+                    _hover={{
+                      color: "gray.400",
+                    }}
+                    wordBreak="break-all"
+                  >
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={item.url}
+                    >
+                      {item.url.replace("https://", "")}
+                    </a>
+                  </Text>
+                </Box>
+                <Spacer />
+
+                <Box mt={[0, -2]} mx="4">
+                  <Text
+                    color={successColor(
+                      successRate(item.success_count, item.total_count)
+                    )}
+                    fontSize={["xl", "2xl", "3xl", "4xl"]}
+                    cursor="grab"
+                  >
+                    <CountUp
+                      end={Number(
+                        successRate(item.success_count, item.total_count)
+                      )}
+                      duration={0.5}
+                    />
+                    %
+                  </Text>
+                </Box>
+              </Flex>
+
+              <Box py="2">
+                <Table borderBottom="none" size="sm" variant="simple">
+                  <Tbody>
+                    <Tr>
+                      <Td>Total Count</Td>
+                      <Td isNumeric>{item.total_count}</Td>
+                    </Tr>
+                    {/* <Tr>
+                      <Td>200</Td>
+                      <Td isNumeric>{item.statuses[200]}</Td>
+                    </Tr> */}
+                    <Tr>
+                      <Td>Success Count</Td>
+                      <Td isNumeric>{item.success_count}</Td>
+                    </Tr>
+                    <Tr>
+                      <Td>Failure Count</Td>
+                      <Td isNumeric>{item.failure_count}</Td>
+                    </Tr>
+                    {/* <Tr>
+                      <Td>Failure Rate</Td>
+                      <Td isNumeric>{item.failure_rate}</Td>
+                    </Tr> */}
+                  </Tbody>
+                </Table>
               </Box>
-              <Box>Total count: {item.total_count}</Box>
-              <Box>200: {item.statuses[200]}</Box>
-              <Box>Success count: {item.success_count}</Box>
-              <Box>Failure count: {item.failure_count}</Box>
-              <Box>Failure rate: {item.failure_rate}</Box>
             </Box>
           </GridBox>
         ))}
