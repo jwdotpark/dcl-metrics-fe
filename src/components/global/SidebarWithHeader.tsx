3ec27f6949823b0b7b3345d20e09eabebe37d802
Jongwoo Park <jongwoo.park@code.berlin>
2022-10-24 13:54:16 +0200

    removed status in the sidebar temporarily

--- src/components/global/SidebarWithHeader.tsx
+++ src/components/global/SidebarWithHeader.tsx
@@ -142,51 +142,6 @@ const SidebarContent = ({
 }: SidebarProps) => {
   const router = useRouter()
 
-  const sidebarList = {
-    global: {
-      label: "Global Dashboard",
-      name: "",
-      icon: FiTrendingUp,
-      subItem: false,
-    },
-    users: {
-      label: "Users",
-      name: "users",
-      icon: FiUsers,
-      subItem: true,
-    },
-    scenes: {
-      label: "Scenes",
-      name: "scenes",
-      icon: FiMapPin,
-      subItem: true,
-    },
-    parcels: {
-      label: "Parcels",
-      name: "parcels",
-      icon: FiPackage,
-      subItem: true,
-    },
-    status: {
-      label: "Status",
-      name: "status",
-      icon: FiActivity,
-      subItem: false,
-    },
-    roadmap: {
-      label: "Roadmap",
-      name: "roadmap",
-      icon: FiAnchor,
-      subItem: false,
-    },
-    about: {
-      label: "About",
-      name: "about",
-      icon: FiCompass,
-      subItem: false,
-    },
-  }
-
   const SidebarItem = ({ label, name, icon, subItem }) => {
     return (
       <Tooltip
