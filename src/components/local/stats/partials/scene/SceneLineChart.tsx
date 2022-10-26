55a604aa6b4a5c95be1ecfc6db84fa991c54b209
Jongwoo Park <jongwoo.park@code.berlin>
2022-10-26 13:47:05 +0200

    added clamp on scene line chart

--- src/components/local/stats/partials/scene/SceneLineChart.tsx
+++ src/components/local/stats/partials/scene/SceneLineChart.tsx
@@ -93,6 +93,10 @@ const MyResponsiveLine = ({ res, selectedScene }) => {
     currentScene.data.map((item) => item.y)
   )
 
+  console.log(currentMax)
+  console.log(currentMin)
+  console.log(currentScene)
+
   return (
     <Tooltip
       p="2"
@@ -115,17 +119,17 @@ const MyResponsiveLine = ({ res, selectedScene }) => {
         shadow="md"
       >
         <ResponsiveLine
-          // layers={[BarLegend]}
           data={memoizedData}
           colors={colors}
           margin={{ top: 30, right: 20, bottom: 60, left: 50 }}
-          xScale={{ type: "point" }}
+          xScale={{ type: "linear" }}
           yScale={{
             type: "linear",
             min: currentMin,
             max: currentMax,
             stacked: false,
             reverse: false,
+            clamp: "auto",
           }}
           yFormat=" >-.2f"
           axisRight={null}
