4b66f56f653c6096f3019fa03ba1265ba962610f
Jongwoo Park <jongwoo.park@code.berlin>
2022-10-26 16:34:14 +0200

    removed unused code

--- pages/_error.js
+++ pages/_error.js
@@ -1,4 +1,3 @@
-// import * as Sentry from "@sentry/nextjs"
 import NextErrorComponent from "next/error"
 import Layout from "../src/components/layout/layout"
 
@@ -11,7 +10,6 @@ const CustomErrorComponent = (props) => {
 }
 
 CustomErrorComponent.getInitialProps = async (contextData) => {
-  // await Sentry.captureUnderscoreErrorException(contextData)
   return NextErrorComponent.getInitialProps(contextData)
 }
 
