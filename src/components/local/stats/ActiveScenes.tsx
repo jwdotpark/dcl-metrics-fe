c8c25332f74b8bf0c7d1f273a2ce9070221d2b6c
Jongwoo Park <jongwoo.park@code.berlin>
2022-10-28 17:03:34 +0200

    umami event name changed

--- src/components/local/stats/ActiveScenes.tsx
+++ src/components/local/stats/ActiveScenes.tsx
@@ -106,7 +106,7 @@ const ActiveScenes = ({ visitorLoading, data }) => {
           dateRange={dateRange}
           setDateRange={setDateRange}
           validLegnth={validLegnth}
-          name="global.scenes_visited"
+          name="global_scenes_visited"
         />
         {chartData.length > 0 && !visitorLoading ? (
           <Box h="100%">
