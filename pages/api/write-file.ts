55533a0069b213747ed93f2fc271aa1f700b0b2d
Jongwoo Park <jongwoo.park@code.berlin>
2022-11-13 17:54:41 +0100

    wip: writing file #4

--- pages/api/write-file.ts
+++ pages/api/write-file.ts
@@ -1,19 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import fs from "fs"
+import { writeFile } from "fs"
 
 // save body to json file in /public/data
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { data, name } = req.body
   console.log("write-file", data, name)
+  const path = "./public/data/test.json"
   if (req.method === "POST" && name === "global") {
     // write file
-    fs.writeFileSync("./public/data/test.json", JSON.stringify(data, null, 2))
-    // read file
-    const file = fs.readFileSync("./public/data/test.json", "utf8")
-    return res
-      .status(200)
-      .json({ message: "Static cache created", data: JSON.parse(file) })
+    writeFile(path, JSON.stringify(data, null, 2), (error) => {
+      if (error) {
+        console.error(error)
+        return
+      }
+      console.log("File has been created")
+    })
   }
+
+  // read file
+  const file = fs.readFile(path, "utf8", (err, jsonString) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(jsonString)
+  })
+
+  return res.status(200).json({ message: "Static cache created", data: file })
 }
 
 export default handler
