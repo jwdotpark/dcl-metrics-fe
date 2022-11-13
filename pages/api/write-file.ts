a43c746f3a6bbc532a87b047181ceb429e25e00d
Jongwoo Park <jongwoo.park@code.berlin>
2022-11-13 17:17:51 +0100

    wip: writing file

--- pages/api/write-file.ts
+++ pages/api/write-file.ts
@@ -4,13 +4,18 @@ import fs from "fs"
 // save body to json file in /public/data
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { data, name } = req.body
-
+  console.log("write-file", data, name)
   if (req.method === "POST" && name === "global") {
     fs.writeFileSync(
       `./public/data/cached_global_response_temp.json`,
       JSON.stringify(data)
     )
-    return res.status(200).json({ message: "Static cache created" })
+    const cache = fs.readFileSync(
+      `./public/data/cached_global_response_temp.json`
+    )
+    return res
+      .status(200)
+      .json({ message: "Static cache created", data: cache })
   }
 }
 
