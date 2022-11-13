59b4b46ee67a9f500deb483ebdfa33ebb1aa235a
Jongwoo Park <jongwoo.park@code.berlin>
2022-11-13 18:21:42 +0100

    wip: writing file #7

--- pages/api/write-file.ts
+++ pages/api/write-file.ts
@@ -6,17 +6,16 @@ import { writeFile } from "fs"
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { data, name } = req.body
   if (req.method === "POST") {
-    try {
-      fs.writeFileSync("./example.json", JSON.stringify(data))
-      return res.status(200).json({ message: "success" })
-    } catch (error) {
-      return res.status(500).json({ error: "error" })
-    } finally {
-      const file = fs.readFileSync("./example.json", "utf8")
-      return res
-        .status(200)
-        .json({ message: `${name} static cache created`, data: file })
-    }
+    const path = "./example.json"
+
+    // write
+    fs.writeFileSync(path, JSON.stringify(data))
+    // read
+    const file = fs.readFileSync(path, "utf8")
+    console.log(file)
+    res
+      .status(200)
+      .json({ message: `${name} static cache created`, data: file })
   }
 }
 
