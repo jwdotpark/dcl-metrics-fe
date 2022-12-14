a457fe26e0454f6c2b654d7acbded926c9dceb8c
Jongwoo Park <jongwoo.park@code.berlin>
2022-12-14 15:03:38 +0100

    404 page added

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,42 @@
-import { Center, Text } from "@chakra-ui/react"
+import {
+  Button,
+  VStack,
+  Center,
+  Text,
+  Box,
+  Flex,
+  Spacer,
+} from "@chakra-ui/react"
 import Layout from "../src/components/layout/layout"
+import { useRouter } from "next/router"
 
 const NotFoundPage = () => {
+  const router = useRouter()
+  const url = router.asPath
+  console.log(router)
   return (
     <Layout>
       <Center h="calc(100vh - 8rem)">
-        <Text fontSize="6xl">404</Text>
+        <VStack dir="row">
+          <Box>
+            <Text fontSize="8xl">404</Text>
+          </Box>
+          <Spacer />
+          <Center w={["60%", "100%"]}>
+            <Text fontSize="3xl">{url} does not exist!</Text>
+          </Center>
+          <Center mx="4" pt="8">
+            <Button
+              borderRadius="xl"
+              colorScheme="gray"
+              onClick={() => router.push("/")}
+              size="sm"
+              variant="solid"
+            >
+              Go back to home
+            </Button>
+          </Center>
+        </VStack>
       </Center>
     </Layout>
   )
