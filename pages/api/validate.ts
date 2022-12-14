74d26289b02836420f2b10b76267eb036ed72bc5
Jongwoo Park <jongwoo.park@code.berlin>
2022-12-14 18:59:14 +0100

    login cred validation for edifice added

--- pages/api/validate.ts
+++ pages/api/validate.ts
@@ -5,11 +5,15 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   const ups_pw = process.env.UPS_PW
   const goldfish = process.env.GOLDFISH
   const goldfish_pw = process.env.GOLDFISH_PW
+  const edifice = process.env.EDIFICE
+  const edifice_pw = process.env.EDIFICE_PW
+
   const { account, password } = req.body
-  
+
   if (
     (account === ups && password === ups_pw) ||
-    (account === goldfish && password === goldfish_pw)
+    (account === goldfish && password === goldfish_pw) ||
+    (account === edifice && password === edifice_pw)
   ) {
     return res.status(200).json({ isAuthenticated: true })
   } else {
