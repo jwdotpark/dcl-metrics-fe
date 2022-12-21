b8c87a6147286d362969e4d5cf79d9627b765f30
Jongwoo Park <jongwoo.park@code.berlin>
2022-12-21 16:22:12 +0100

    avg box mobile layout fixed

--- src/components/local/stats/partials/AvgStat.tsx
+++ src/components/local/stats/partials/AvgStat.tsx
@@ -16,43 +16,39 @@ const AvgStat = ({ avg, data, color }) => {
     <Box>
       <Box>
         <Flex>
-          {avg.length > 1 && (
-            <Tooltip
-              p="2"
-              fontSize="sm"
-              borderRadius="xl"
-              label={`Average value for ${data.length} days`}
-              placement="top"
-            >
-              <Box w="100%" pt="1">
-                <FiInfo size="14px" />
-              </Box>
-            </Tooltip>
-          )}
           {typeof avg === "object" &&
             avg.map((item, i) => {
               return (
                 <Box
                   key={item.id}
                   w="100%"
-                  minW={[0, 0, 75, 100]}
+                  minW={[0, 0, 100, 100]}
                   color="gray.500"
                   fontSize="sm"
                 >
-                  <Flex direction="column" minW={[0, 0, 75, 100]}>
+                  <Flex direction="column" minW={[0, 0, 100, 100]}>
+                    <Tooltip
+                      p="2"
+                      fontSize="sm"
+                      borderRadius="xl"
+                      label={`Average ${item.id} value for ${data.length} days`}
+                      placement="top"
+                    >
+                      <Box
+                        mr="2"
+                        color={color[i]}
+                        fontSize={["xl", "xl", "2xl", "2xl"]}
+                        fontWeight="bold"
+                        textAlign={["start", "start", "end", "end"]}
+                        _hover={{ cursor: "pointer" }}
+                      >
+                        <CountUp end={item.value} duration={0.5} />
+                      </Box>
+                    </Tooltip>
                     <Box mr="2" textAlign={["start", "start", "end", "end"]}>
-                      <Text fontSize="sm" noOfLines={1}>
+                      <Text fontSize={[10, "sm"]} noOfLines={1}>
                         {item.id}
                       </Text>
-                    </Box>
-                    <Box
-                      mr="2"
-                      color={color[i]}
-                      fontSize={["xl", "xl", "2xl", "2xl"]}
-                      fontWeight="bold"
-                      textAlign={["start", "start", "end", "end"]}
-                    >
-                      <CountUp end={item.value} duration={0.5} />
                     </Box>
                   </Flex>
                 </Box>
