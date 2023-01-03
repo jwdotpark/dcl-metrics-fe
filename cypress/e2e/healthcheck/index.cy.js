aa77c627cac2c9267f1e80ed166237a10c247158
Jongwoo Park <jongwoo.park@code.berlin>
2023-01-03 18:33:18 +0100

    e2e init

---
+++ cypress/e2e/healthcheck/index.cy.js
@@ -0,0 +1,20 @@
+const { cy } = require("date-fns/locale")
+
+describe("Index page", () => {
+  it("should display components", () => {
+    cy.visit("http://localhost:3000/")
+
+    cy.contains("Unique Visitors")
+    cy.contains("Parcels Visitors")
+    cy.contains("Scenes Visited")
+    cy.contains("Land Picker")
+    cy.contains("Marathon Users")
+    cy.contains("Explorers")
+    cy.contains("Scenes")
+    cy.contains("Scenes with Most Unique Visitors")
+    cy.contains("Scenes with AVG Time Spent")
+    cy.contains("Parcels Average Time Spent")
+    cy.contains("Most Visited Parcel")
+    cy.contains("okgu")
+  })
+})
