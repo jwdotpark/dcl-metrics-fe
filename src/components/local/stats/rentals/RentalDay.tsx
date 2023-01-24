441a698b5d03049e27ddd8dc45e15d01c51afdb3
Jongwoo Park <jongwoo.park@code.berlin>
2023-01-24 20:13:20 +0100

    wip: temp rent total pie chart

--- src/components/local/stats/rentals/RentalDay.tsx
+++ src/components/local/stats/rentals/RentalDay.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import { Box, color } from "@chakra-ui/react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { defaultDateRange, sliceData } from "../../../../lib/data/chartInfo"
 import LineChart from "../../../../lib/LineChart"
 import BoxTitle from "../../../layout/local/BoxTitle"
@@ -12,12 +12,14 @@ const RentalDay = ({ data }) => {
   const dataArr = Object.entries(data)
   const chartData = []
   const color = ["#48BB78", "#4299E1", "#9F7AEA", "#F56565"]
-  const [dateRange, setDateRange] = useState(defaultDateRange)
+  const [dateRange, setDateRange] = useState(31)
   const [avgData, setAvgData] = useState([])
 
+  // TODO clarify these value with later
   const value = 1000000000000000000
   const value2 = 100000000000000000
   const value3 = 100
+
   dataArr.map((item) => {
     chartData.push({
       id: item[1].date,
@@ -50,14 +52,48 @@ const RentalDay = ({ data }) => {
     mapData("Rentals", "rentals"),
   ]
 
+  const calculateAverages = (partial) => {
+    const validLength = partial.length
+    const sum = {
+      volume: partial.reduce((acc, cur) => acc + cur.volume, 0),
+      lessorEarnings: partial.reduce((acc, cur) => acc + cur.lessorEarnings, 0),
+      feeCollectorEarnings: partial.reduce(
+        (acc, cur) => acc + cur.feeCollectorEarnings,
+        0
+      ),
+      rentals: partial.reduce((acc, cur) => acc + cur.rentals, 0),
+    }
+
+    const value = {
+      volume: Math.round(sum.volume / validLength),
+      lessorEarnings: Math.round(sum.lessorEarnings / validLength),
+      feeCollectorEarnings: Math.round(sum.feeCollectorEarnings / validLength),
+      rentals: Math.round(sum.rentals / validLength),
+    }
+
+    const map = [
+      { id: "Volume", value: value.volume },
+      { id: "Lessor Earnings", value: value.lessorEarnings },
+      { id: "Fee Collector Earnings", value: value.feeCollectorEarnings },
+      { id: "Rentals", value: value.rentals },
+    ].sort((a, b) => b.value - a.value)
+
+    return map
+  }
+
+  useEffect(() => {
+    setAvgData(calculateAverages(partial))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dateRange])
+
   return (
     <Box w={["100%", "100%"]} mr={[0, 4]}>
       <BoxWrapper>
         <BoxTitle
           name="Rental Day"
           date={""}
-          avgData={""}
-          slicedData={{}}
+          avgData={avgData}
+          slicedData={partial}
           color={color}
           description="Daily rental data"
         />
--- src/components/local/stats/rentals/RentalTotal.tsx
+++ src/components/local/stats/rentals/RentalTotal.tsx
@@ -1,10 +1,54 @@
 import { Box } from "@chakra-ui/react"
+import BoxTitle from "../../../layout/local/BoxTitle"
 import BoxWrapper from "../../../layout/local/BoxWrapper"
+import PieChart from "../../../../lib/PieChart"
 
 const RentalTotal = ({ data }) => {
+  console.log(data)
+  const chartData = []
+  const color = ["#48BB78", "#4299E1", "#9F7AEA", "#F56565"]
+
+  data.map((item) => {
+    chartData.push({
+      id: item.__typename,
+      feeCollectorEarnings: item.feeCollectorEarnings,
+      lessorEarnings: item.lessorEarnings,
+      rentals: item.rentals,
+      volume: item.volume,
+    })
+  })
+
+  console.log("chart data", chartData)
+
+  const mapData = (id: string, key: number) => {
+    return {
+      id: id,
+      label: id,
+      value: key,
+      color: color[key],
+    }
+  }
+
+  const result = [
+    mapData("Volume", chartData[0].volume),
+    mapData("Lessor Earnings", chartData[0].lessorEarnings),
+    mapData("Fee Collector Earnings", chartData[0].feeCollectorEarnings),
+    mapData("Rentals", chartData[0].rentals),
+  ]
+
   return (
     <Box w={["100%", "50%"]}>
-      <BoxWrapper>asdf</BoxWrapper>
+      <BoxWrapper>
+        <BoxTitle
+          name="Rental Total"
+          date={""}
+          avgData={""}
+          slicedData={""}
+          color={""}
+          description="Total rental data"
+        />
+        <PieChart data={result} />
+      </BoxWrapper>
     </Box>
   )
 }
