fd7558ec17a893274101b82bb2507b87808715ec
Jongwoo Park <jongwoo.park@code.berlin>
2023-01-29 13:38:23 +0100

    csv download btn added

--- src/components/layout/local/SceneTitle.tsx
+++ src/components/layout/local/SceneTitle.tsx
@@ -1,7 +1,32 @@
-import { Box, Text, Flex, Spacer, Button, IconButton } from "@chakra-ui/react"
-import AvgStat from "../../local/stats/partials/AvgStat"
+import {
+  Box,
+  Text,
+  Flex,
+  Spacer,
+  IconButton,
+  Tooltip,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  ButtonGroup,
+} from "@chakra-ui/react"
 import DatePicker from "../../local/stats/scenes/DatePicker"
 import { FiDownload } from "react-icons/fi"
+import { getEndpoint } from "../../../lib/data/constant"
+import React from "react"
 
 const SceneTitle = ({
   name,
@@ -13,7 +38,22 @@ const SceneTitle = ({
   description,
   uuid,
 }) => {
-  console.log(uuid)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const handleCSVClick = () => {
+    onOpen()
+  }
+
+  const handleSaveClick = () => {
+    const path = `reports/${uuid}`
+    const endpoint = getEndpoint(path)
+    window.open(endpoint)
+    onClose()
+  }
+
+  const handleCloseClick = () => {
+    onClose()
+  }
+
   return (
     <Flex direction={["column", "column", "row", "row"]}>
       <Box>
@@ -34,15 +74,54 @@ const SceneTitle = ({
       {uuid.length > 0 && (
         <Flex m="4">
           <Box mr="2">
-            <IconButton
-              sx={{ transform: "translateY(1px)" }}
-              border="2px solid"
-              borderRadius="lg"
-              shadow="md"
-              aria-label="Download"
-              icon={<FiDownload />}
-              variant="outline"
-            />
+            <Modal isCentered isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent borderRadius="xl">
+                <ModalHeader></ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <Box w="100%">
+                    <Text fontSize="lg" wordBreak="break-word">
+                      Would you like to download the CSV data of
+                      <br />
+                      <kbd>{name}</kbd>?
+                    </Text>
+                  </Box>
+                </ModalBody>
+                <ModalFooter>
+                  <ButtonGroup>
+                    <Button
+                      borderRadius="xl"
+                      colorScheme="green"
+                      onClick={handleSaveClick}
+                    >
+                      Download
+                    </Button>
+                    <Button borderRadius="xl" onClick={handleCloseClick}>
+                      Cancel
+                    </Button>
+                  </ButtonGroup>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+            <Tooltip
+              p="2"
+              fontSize="sm"
+              borderRadius="md"
+              label="Download CSV"
+              placement="top"
+            >
+              <IconButton
+                sx={{ transform: "translateY(1px)" }}
+                border="2px solid"
+                borderRadius="lg"
+                shadow="md"
+                aria-label="Download"
+                icon={<FiDownload />}
+                onClick={handleCSVClick}
+                variant="outline"
+              />
+            </Tooltip>
           </Box>
           <Box w="100%">
             <DatePicker
