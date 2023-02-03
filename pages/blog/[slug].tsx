83a8b9fb94b8d43eedc12b7aff5c831a73125971
Jongwoo Park <jongwoo.park@code.berlin>
2023-02-03 18:35:47 +0100

    wip: adding formatter on slug page

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -5,32 +5,47 @@ import { serialize } from "next-mdx-remote/serialize"
 import Layout from "../../src/components/layout/layout"
 import BoxWrapper from "../../src/components/layout/local/BoxWrapper"
 import { useRouter } from "next/router"
+import externalLinks from "remark-external-links"
+import smartypants from "remark-smartypants"
+import remarkGfm from "remark-gfm"
 
 function Post({ data, content }) {
   const router = useRouter()
   return (
     <Layout>
-      <BoxWrapper colSpan={6}>
-        <Box>
-          <Box m="4">
-            <Text>{data.title}</Text>
-            <Text>{data.date}</Text>
-          </Box>
-          <Box m="4">
-            <MDXRemote {...content} />
-          </Box>
-          <Box m="4">
-            <Button
-              w="100%"
-              onClick={() => {
-                router.push("/blog")
-              }}
-            >
-              Go Back
-            </Button>
-          </Box>
-        </Box>
-      </BoxWrapper>
+      <Center>
+        <Center w={["100%", "100%", "100%", 1080]}>
+          <BoxWrapper colSpan={6}>
+            <Box>
+              <Flex m="4" mb="8">
+                <Box w={["70%", "85%"]}>
+                  <Text fontSize={["lg", "xl", "2xl", "3xl"]} fontWeight="bold">
+                    {data.title}
+                  </Text>
+                </Box>
+                <Spacer />
+                <Center h="auto">
+                  <Text wordBreak="keep-all">{data.date}</Text>
+                </Center>
+              </Flex>
+
+              <Box m="4" mx="8">
+                <MDXRemote {...content} />
+              </Box>
+              <Box m="4">
+                <Button
+                  w="100%"
+                  onClick={() => {
+                    router.push("/blog")
+                  }}
+                >
+                  Go Back
+                </Button>
+              </Box>
+            </Box>
+          </BoxWrapper>
+        </Center>
+      </Center>
     </Layout>
   )
 }
@@ -48,7 +63,19 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params }) => {
   const post = await getPost(params.slug)
-  const mdxSource = await serialize(post.content)
+  const mdxSource = await serialize(post.content, {
+    scope: post.data,
+    mdxOptions: {
+      remarkPlugins: [
+        [externalLinks, { target: "_blank" }],
+        smartypants,
+        remarkGfm,
+      ],
+      rehypePlugins: [],
+      format: "mdx",
+    },
+  })
+
   return {
     props: {
       data: post.data,
