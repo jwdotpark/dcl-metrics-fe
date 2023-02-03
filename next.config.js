5803b0d31f159194599097a793ce64eec16ce380
Jongwoo Park <jongwoo.park@code.berlin>
2023-02-03 18:45:36 +0100

    wip: adding formatter on blog slug page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+//import externalLinks from "remark-external-links"
+//import smartypants from "remark-smartypants"
+//import remarkGfm from "remark-gfm"
+
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   swcMinify: true,
   images: {
     domains: ["https://api.decentraland.org/", "picsum.photos"],
@@ -9,4 +25,4 @@ const nextConfig = {
   staticPageGenerationTimeout: 60000,
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
