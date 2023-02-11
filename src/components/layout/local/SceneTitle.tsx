f56e2232c2a222a1aecc65e724bd3803df450b73
Jongwoo Park <jongwoo.park@code.berlin>
2023-02-11 07:18:53 +0100

    wip: fixing csv download

--- src/components/layout/local/SceneTitle.tsx
+++ src/components/layout/local/SceneTitle.tsx
@@ -19,7 +19,8 @@ import {
 import DatePicker from "../../local/stats/scenes/DatePicker"
 import { FiDownload } from "react-icons/fi"
 import { getEndpoint } from "../../../lib/data/constant"
-import React from "react"
+import React, { useState } from "react"
+import { getDataWithProxy } from "../../../lib/data/fetch"
 
 const SceneTitle = ({
   name,
@@ -32,14 +33,32 @@ const SceneTitle = ({
   uuid,
 }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+
   const handleCSVClick = () => {
     onOpen()
   }
 
-  const handleSaveClick = () => {
+  const handleSaveClick = async () => {
     const path = `reports/${uuid}`
     const endpoint = getEndpoint(path)
-    window.open(endpoint)
+
+    const response = await fetch(endpoint, {
+      headers: {
+        Accept: "text/csv",
+      },
+      mode: "no-cors",
+    })
+
+    const csvData = await response.text()
+    console.log(csvData)
+
+    const blob = new Blob([csvData], { type: "text/csv" })
+
+    const tempLink = document.createElement("a")
+    tempLink.href = URL.createObjectURL(blob)
+    tempLink.download = "report.csv"
+    tempLink.click()
+
     onClose()
   }
 
@@ -88,7 +107,12 @@ const SceneTitle = ({
                       colorScheme="green"
                       onClick={handleSaveClick}
                     >
+                      {/*<a
+                        href={`${getEndpoint(`reports/${uuid}`)}`}
+                        download={`${name}.csv`}
+                      >*/}
                       Download
+                      {/*</a>*/}
                     </Button>
                     <Button borderRadius="xl" onClick={handleCloseClick}>
                       Cancel
