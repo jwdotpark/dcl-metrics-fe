c6d2a1d62b8c98a7142bd90d837ed78a29cc50f3
Jongwoo Park <jongwoo.park@code.berlin>
2023-03-15 19:36:59 +0100

    chart component bottom legend color changed

--- src/components/local/dcl-data/partial/BottomLegend.tsx
+++ src/components/local/dcl-data/partial/BottomLegend.tsx
@@ -3,11 +3,7 @@ import { Text, Center, useColorModeValue } from "@chakra-ui/react"
 const BottomLegend = ({ description }) => {
   return (
     <Center mb="2">
-      <Text
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        color={useColorModeValue("gray.100", "gray.600")}
-        fontSize="xs"
-      >
+      <Text color="gray.400" fontSize="xs">
         {description}
       </Text>
     </Center>
