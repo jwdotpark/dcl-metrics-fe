a1af0a33b396e43af5076bd40b3ec88a15b300ec
Jongwoo Park <jongwoo.park@code.berlin>
2023-03-23 15:23:40 +0100

    MD video component added

--- src/components/markdown/index.tsx
+++ src/components/markdown/index.tsx
@@ -1,9 +1,28 @@
-import { Box, useColorModeValue } from "@chakra-ui/react"
+import { Box, Text, useColorModeValue } from "@chakra-ui/react"
 
 export const CallOut = ({ children }) => {
   return (
-    <Box p="4" bg={useColorModeValue("gray.200", "gray.600")} borderRadius="xl">
-      {children}
+    <Box
+      mb="16"
+      p="8"
+      bg={useColorModeValue("gray.200", "gray.600")}
+      borderRadius="xl"
+    >
+      <Text fontSize="xl">{children}</Text>
+    </Box>
+  )
+}
+
+export const MDYoutube = ({ id }) => {
+  return (
+    <Box overflow="clip" my="4" borderRadius="xl">
+      <iframe
+        width="100%"
+        height="450"
+        src={"https://www.youtube.com/embed/" + id}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      ></iframe>
     </Box>
   )
 }
