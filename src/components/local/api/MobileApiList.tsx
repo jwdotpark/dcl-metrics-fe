c4a0a8a5df63075370c42d3726ae49b5daa6ded5
Jongwoo Park <jongwoo.park@code.berlin>
2023-03-24 15:25:05 +0100

    removed code

--- src/components/local/api/MobileApiList.tsx
+++ src/components/local/api/MobileApiList.tsx
@@ -70,39 +70,6 @@ const ApiListDrawer = (props) => {
                                 i={item.data.issue}
                               />
                             </Box>
-                            //<Link
-                            //  href={`/api-docs/${item.data.category}-${item.data.title}`}
-                            //  key={item.data.title}
-                            //  onClick={onClose}
-                            //>
-                            //  <ListItem
-                            //    key={item.data.title}
-                            //    w="100%"
-                            //    mb="2"
-                            //    px="4"
-                            //    py="2"
-                            //    bg={
-                            //      itemIndex === i &&
-                            //      // eslint-disable-next-line react-hooks/rules-of-hooks
-                            //      useColorModeValue("gray.200", "gray.700")
-                            //    }
-                            //    borderRadius="xl"
-                            //    _hover={{
-                            //      // eslint-disable-next-line react-hooks/rules-of-hooks
-                            //      bg: useColorModeValue("gray.200", "gray.600"),
-                            //      cursor: "pointer",
-                            //    }}
-                            //    onClick={() => {
-                            //      setSelectedItem(item)
-                            //      setItemIndex(i)
-                            //    }}
-                            //  >
-                            //    <Text fontSize="lg" fontWeight="semibold">
-                            //      {item.data.title}
-                            //    </Text>
-                            //    <Text as="kbd">{item.data.description}</Text>
-                            //  </ListItem>
-                            //</Link>
                           )
                         }
                       })}
