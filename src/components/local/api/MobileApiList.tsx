5c45196d84b901fc4d53f620150150ac9572ba2f
Jongwoo Park <jongwoo.park@code.berlin>
2023-03-24 12:25:41 +0100

    api list spacing added

--- src/components/local/api/MobileApiList.tsx
+++ src/components/local/api/MobileApiList.tsx
@@ -71,7 +71,8 @@ const ApiListDrawer = (props) => {
                               <ListItem
                                 key={item.data.title}
                                 w="100%"
-                                //px="4"
+                                mb="2"
+                                px="4"
                                 py="2"
                                 bg={
                                   itemIndex === i &&
