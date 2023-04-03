0946a1761973e942ddbb3e57fe9162efedfe7d3f
Jongwoo Park <jongwoo.park@code.berlin>
2023-04-03 16:11:26 +0200

    removed react18 createRoot issue on testing library

--- __tests__/usePrev.test.js
+++ __tests__/usePrev.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks"
+import { renderHook } from "@testing-library/react"
 import { usePrev } from "../src/lib/hooks/usePrev"
 
 describe("usePrev", () => {
