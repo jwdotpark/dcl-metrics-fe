c26435bdf0902e63186a9218122b95f61eab892d
Jongwoo Park <jongwoo.park@code.berlin>
2023-04-03 15:48:38 +0200

    removed unused dependencies

--- src/components/local/api/ApiExample.tsx
+++ src/components/local/api/ApiExample.tsx
@@ -1,33 +1,10 @@
-import {
-  Box,
-  Divider,
-  OrderedList,
-  Text,
-  UnorderedList,
-  useColorModeValue,
-} from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 import BoxWrapper from "../../layout/local/BoxWrapper"
 import ChakraUIRenderer from "chakra-ui-markdown-renderer"
 import ReactMarkdown from "react-markdown"
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
-import { oneDark, oneLight } from "../../../lib/data/syntaxHighlight"
 
 const ApiExample = ({ selectedItem }) => {
   const content = selectedItem.content
-
-  //const customized = {
-  //  code,
-  //  h1,
-  //  h2,
-  //  h3,
-  //  h4,
-  //  h5,
-  //  h6,
-  //  ol,
-  //  p,
-  //  ul,
-  //}
-
   return (
     <BoxWrapper colSpan={[6, 6, 6, 4, 4]}>
       <Box p="4">
