656ad4ff1bd55047f7332a00335f34fc09ae0c43
Jongwoo Park <jongwoo.park@code.berlin>
2023-04-03 18:17:53 +0200

    linechart smoke testing added

---
+++ __tests__/utils/mocks.js
@@ -0,0 +1,53 @@
+export const blogMockFiles = ["post1.mdx", "post2.mdx", "post3.mdx"]
+export const blogMockPostSlug = "post1"
+export const blogMockPostData = [
+  {
+    slug: "post1",
+    data: { title: "Post 1", date: "2022-01-01" },
+    content: "This is the content of post 1.",
+  },
+  {
+    slug: "post2",
+    data: { title: "Post 2", date: "2022-01-02" },
+    content: "This is the content of post 2.",
+  },
+  {
+    slug: "post3",
+    data: { title: "Post 3", date: "2022-01-03" },
+    content: "This is the content of post 3.",
+  },
+]
+
+export const lineChartData = [
+  {
+    id: "line1",
+    data: [
+      { x: "2022-03-01", y: 30 },
+      { x: "2022-03-02", y: 50 },
+      { x: "2022-03-03", y: 100 },
+    ],
+  },
+  {
+    id: "line2",
+    data: [
+      { x: "2022-03-01", y: 20 },
+      { x: "2022-03-02", y: 80 },
+      { x: "2022-03-03", y: 40 },
+    ],
+  },
+]
+
+export const lineChartRentalData = {
+  data: [
+    { date: "2022-03-01", y: 10 },
+    { date: "2022-03-02", y: 15 },
+    { date: "2022-03-03", y: 20 },
+  ],
+}
+
+export const lineChartAvgColor = "#000"
+export const lineChartAvgData = [
+  { x: "2022-03-01", y: 20 },
+  { x: "2022-03-02", y: 65 },
+  { x: "2022-03-03", y: 70 },
+]
