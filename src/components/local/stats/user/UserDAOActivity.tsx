c25ae1907b1b5192ef488c8a55ff8cde324d085a
Jongwoo Park <jongwoo.park@code.berlin>
2023-04-12 20:25:57 +0200

    adding more properties on dao activity

--- src/components/local/stats/user/UserDAOActivity.tsx
+++ src/components/local/stats/user/UserDAOActivity.tsx
@@ -33,6 +33,8 @@ import { useRouter } from "next/router"
 import Link from "next/link"
 import ToolTip from "../../../layout/local/ToolTip"
 import moment from "moment"
+import UserDAOActivityCollection from "./partial/UserDAOActivityCollection"
+import UserDAOActivityDelegators from "./partial/UserDAOActivityDelegators"
 
 const UserDAOActivity = ({ data }) => {
   const {
@@ -44,7 +46,6 @@ const UserDAOActivity = ({ data }) => {
     address,
     collection_creator,
     collections,
-    dao_member,
     delegate,
     delegated_vp,
     delegators,
@@ -52,8 +53,6 @@ const UserDAOActivity = ({ data }) => {
     proposals,
     teams,
   } = data
-
-  const router = useRouter()
 
   const responsiveStr = useBreakpointValue({
     xs: 5,
@@ -70,21 +69,6 @@ const UserDAOActivity = ({ data }) => {
       return name.slice(0, nameLength) + ".."
     }
     return name
-  }
-
-  console.log(collections)
-
-  const toast = useToast()
-
-  const handleToast = (value) => {
-    navigator.clipboard.writeText(value)
-    toast({
-      description: "Address " + value + " has been copied to the clipboard.",
-      duration: 2000,
-      isClosable: true,
-      position: "bottom-right",
-      status: "success",
-    })
   }
 
   return (
@@ -116,7 +100,24 @@ const UserDAOActivity = ({ data }) => {
               <Spacer />
               <Box>
                 <b>
-                  <CountUp end={total_vp} duration={0.5} decimals={0} />
+                  <CountUp
+                    end={total_vp ? total_vp : "N/A"}
+                    duration={0.5}
+                    decimals={0}
+                  />
+                </b>
+              </Box>
+            </Flex>
+            <Flex w="100%" h="100%">
+              <Box>Delegated VP</Box>
+              <Spacer />
+              <Box>
+                <b>
+                  <CountUp
+                    end={delegated_vp ? delegated_vp : "N/A"}
+                    duration={0.5}
+                    decimals={0}
+                  />
                 </b>
               </Box>
             </Flex>
@@ -169,163 +170,10 @@ const UserDAOActivity = ({ data }) => {
                 </Text>
               </Box>
             </Flex>
-            {/* TODO check if array is not empty */}
-            {collections && (
-              <Flex w="100%" h="100%">
-                <Box>Collection</Box>
-                <Spacer />
-                <Box>
-                  <Text>
-                    <b>
-                      {collections.length > 0 ? (
-                        <Popover placement="left">
-                          <PopoverTrigger>
-                            <Button
-                              borderRadius="xl"
-                              disabled={delegators.length > 0 ? false : true}
-                              size="sm"
-                              variant="solid"
-                            >
-                              {collections.length > 0
-                                ? collections.length
-                                : "N/A"}
-                            </Button>
-                          </PopoverTrigger>
-                          <PopoverContent w="100%" borderRadius="xl">
-                            <PopoverArrow />
-                            <PopoverBody>
-                              {collections.length > 0 &&
-                                collections.map((item) => {
-                                  return (
-                                    <Flex key={item.name} w="100%" mb="2">
-                                      <Box w="100%">
-                                        <Popover placement="left">
-                                          <PopoverTrigger>
-                                            <Box w="100%">
-                                              <Button
-                                                w="100%"
-                                                shadow="md"
-                                                size="sm"
-                                                variant="solid"
-                                              >
-                                                <Center>
-                                                  <Text>{item.name}</Text>
-                                                </Center>
-                                              </Button>
-                                            </Box>
-                                          </PopoverTrigger>
-                                          <PopoverContent>
-                                            <PopoverArrow />
-                                            <PopoverBody>
-                                              <Center w="auto" mb="2">
-                                                <Text>{item.name}</Text>
-                                              </Center>
-                                              <Divider />
-                                              <Box>
-                                                <Table
-                                                  w="100%"
-                                                  size="sm"
-                                                  variant="simple"
-                                                >
-                                                  <Tbody>
-                                                    <Tr>
-                                                      <Td>Symbol</Td>
-                                                      <Td isNumeric>
-                                                        {item.symbol}
-                                                      </Td>
-                                                    </Tr>
-                                                    <Tr>
-                                                      <Td>Items</Td>
-                                                      <Td isNumeric>
-                                                        <Text noOfLines={1}>
-                                                          {item.items}
-                                                        </Text>
-                                                      </Td>
-                                                    </Tr>
-                                                    <Tr>
-                                                      <Td>Collection ID</Td>
-                                                      <Td isNumeric>
-                                                        <ToolTip label="Click to copy the address">
-                                                          <Button
-                                                            borderRadius="xl"
-                                                            onClick={() =>
-                                                              handleToast(
-                                                                item.collection_id
-                                                              )
-                                                            }
-                                                            size="sm"
-                                                          >
-                                                            ID
-                                                          </Button>
-                                                        </ToolTip>
-                                                      </Td>
-                                                    </Tr>
-                                                    <Tr>
-                                                      <Td>Approved</Td>
-                                                      <Td isNumeric>
-                                                        <Text
-                                                          color={
-                                                            item.approved
-                                                              ? "green"
-                                                              : "red"
-                                                          }
-                                                        >
-                                                          {item.approved
-                                                            ? "Yes"
-                                                            : "No"}
-                                                        </Text>
-                                                      </Td>
-                                                    </Tr>
-                                                    <Tr>
-                                                      <Td>Created At</Td>
-                                                      <Td isNumeric>
-                                                        {moment(
-                                                          item.created_at
-                                                        ).format(
-                                                          "YYYY MMM. D hh:mm"
-                                                        )}{" "}
-                                                        UTC
-                                                      </Td>
-                                                    </Tr>
-                                                    <Tr>
-                                                      <Td>Created By</Td>
-                                                      <Td isNumeric>
-                                                        <ToolTip label="Click to copy the address">
-                                                          <Button
-                                                            borderRadius="xl"
-                                                            onClick={() =>
-                                                              handleToast(
-                                                                item.created_by
-                                                              )
-                                                            }
-                                                            size="sm"
-                                                          >
-                                                            Address
-                                                          </Button>
-                                                        </ToolTip>
-                                                      </Td>
-                                                    </Tr>
-                                                  </Tbody>
-                                                </Table>
-                                              </Box>
-                                            </PopoverBody>
-                                          </PopoverContent>
-                                        </Popover>
-                                      </Box>
-                                    </Flex>
-                                  )
-                                })}
-                            </PopoverBody>
-                          </PopoverContent>
-                        </Popover>
-                      ) : (
-                        "N/A"
-                      )}
-                    </b>
-                  </Text>
-                </Box>
-              </Flex>
-            )}
+            <UserDAOActivityCollection
+              collections={collections}
+              delegators={delegators}
+            />
             <Flex w="100%" h="100%">
               <Box>Delegate</Box>
               <Spacer />
@@ -335,50 +183,7 @@ const UserDAOActivity = ({ data }) => {
                 </Text>
               </Box>
             </Flex>
-            {delegators && (
-              <Flex w="100%" h="100%">
-                <Box>Delegators</Box>
-                <Spacer />
-                <Box>
-                  <Popover placement="left">
-                    <PopoverTrigger>
-                      <Button
-                        borderRadius="xl"
-                        disabled={delegators.length > 0 ? false : true}
-                        size="sm"
-                        variant="solid"
-                      >
-                        <b>
-                          {delegators.length > 0 ? delegators.length : "N/A"}
-                        </b>
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent w="auto" borderRadius="xl">
-                      <PopoverArrow />
-                      <PopoverBody>
-                        <Box p="2">
-                          {delegators.length > 0 &&
-                            delegators.map((item) => {
-                              return (
-                                <Flex key={item} mb="2">
-                                  <Button shadow="md" size="sm" variant="solid">
-                                    <Link
-                                      href={`/users/${item}`}
-                                      target="_blank"
-                                    >
-                                      <Text as="kbd">{truncateName(item)}</Text>
-                                    </Link>
-                                  </Button>
-                                </Flex>
-                              )
-                            })}
-                        </Box>
-                      </PopoverBody>
-                    </PopoverContent>
-                  </Popover>
-                </Box>
-              </Flex>
-            )}
+            <UserDAOActivityDelegators delegators={delegators} />
           </VStack>
         </Box>
       </Flex>
---
+++ src/components/local/stats/user/partial/UserDAOActivityCollection.tsx
@@ -0,0 +1,81 @@
+import {
+  useToast,
+  Box,
+  Text,
+  Flex,
+  Spacer,
+  Popover,
+  PopoverTrigger,
+  Button,
+  PopoverContent,
+  PopoverArrow,
+  PopoverBody,
+  Center,
+} from "@chakra-ui/react"
+import CollectionPopOverBody from "./CollectionPopOverBody"
+
+const UserDAOActivityCollection = ({ collections, delegators }) => {
+  console.log(collections)
+  return (
+    <Flex w="100%" h="100%">
+      <Box>Collection</Box>
+      <Spacer />
+      <Box>
+        <Text>
+          <b>
+            {collections && (
+              <Popover placement="left">
+                <PopoverTrigger>
+                  <Button
+                    borderRadius="xl"
+                    disabled={delegators.length > 0 ? false : true}
+                    size="sm"
+                    variant="solid"
+                  >
+                    {collections.length > 0 ? collections.length : "N/A"}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent w="100%" borderRadius="xl">
+                  <PopoverArrow />
+                  <PopoverBody>
+                    {collections.length > 0 &&
+                      collections.map((item) => {
+                        return (
+                          <Flex key={item.name} w="100%" mb="2">
+                            <Box w="100%">
+                              <Popover placement="left">
+                                <PopoverTrigger>
+                                  <Box w="100%">
+                                    <Button
+                                      w="100%"
+                                      shadow="md"
+                                      size="sm"
+                                      variant="solid"
+                                    >
+                                      <Center>
+                                        <Text>{item.name}</Text>
+                                      </Center>
+                                    </Button>
+                                  </Box>
+                                </PopoverTrigger>
+                                <PopoverContent>
+                                  <PopoverArrow />
+                                  <CollectionPopOverBody item={item} />
+                                </PopoverContent>
+                              </Popover>
+                            </Box>
+                          </Flex>
+                        )
+                      })}
+                  </PopoverBody>
+                </PopoverContent>
+              </Popover>
+            )}
+          </b>
+        </Text>
+      </Box>
+    </Flex>
+  )
+}
+
+export default UserDAOActivityCollection
