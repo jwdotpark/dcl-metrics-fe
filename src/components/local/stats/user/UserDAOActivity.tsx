8ab39d2f5bc61eecd718cc147f46af095aa8f0d8
Jongwoo Park <jongwoo.park@code.berlin>
2023-04-19 15:14:34 +0200

    check falsy data on user dashboard

--- src/components/local/stats/user/UserDAOActivity.tsx
+++ src/components/local/stats/user/UserDAOActivity.tsx
@@ -1,4 +1,13 @@
-import { Flex, Box, Text, VStack, Spacer } from "@chakra-ui/react"
+/* eslint-disable react-hooks/rules-of-hooks */
+import {
+  Flex,
+  useColorModeValue,
+  Box,
+  Text,
+  VStack,
+  Spacer,
+  Center,
+} from "@chakra-ui/react"
 import CountUp from "react-countup"
 import BoxTitle from "../../../layout/local/BoxTitle"
 import BoxWrapper from "../../../layout/local/BoxWrapper"
@@ -16,7 +25,6 @@ const UserDAOActivity = ({ data }) => {
     total_vp,
     votes,
     active_dao_committee_member,
-    address,
     collection_creator,
     collections,
     delegate,
@@ -26,6 +34,26 @@ const UserDAOActivity = ({ data }) => {
     proposals,
     teams,
   } = data
+
+  const checkFalsyData = () => {
+    if (
+      !total_vp &&
+      !votes &&
+      !active_dao_committee_member &&
+      !collection_creator &&
+      !collections &&
+      !delegate &&
+      !delegated_vp &&
+      !delegators &&
+      !grants &&
+      !proposals &&
+      !teams
+    ) {
+      return false
+    } else {
+      return true
+    }
+  }
 
   return (
     <BoxWrapper colSpan={[1, 1, 1, 2, 2]}>
@@ -39,96 +67,108 @@ const UserDAOActivity = ({ data }) => {
         line={false}
         setLine={{}}
       />
-      <Flex direction="column" w="auto" m="4" mx="5">
-        <Box w="100%">
-          <VStack align="stretch" spacing={[2, 2, 2, 5, 5]}>
-            <Flex w="100%" h="100%">
-              <Box>Title</Box>
-              <Spacer />
-              <Box>
-                <Text>{title ? title : "N/A"}</Text>
-              </Box>
-            </Flex>
-            {total_vp > 0 && (
-              <Flex w="100%" h="100%">
-                <Box>Total VP</Box>
-                <Spacer />
-                <Box>{total_vp ? Math.round(total_vp) : "N/A"}</Box>
-              </Flex>
-            )}
-            {delegated_vp !== 0 && (
-              <Flex w="100%" h="100%">
-                <Box>Delegated VP</Box>
-                <Spacer />
-                <Box>{delegated_vp ? delegated_vp : "N/A"}</Box>
-              </Flex>
-            )}
-            {votes && (
-              <>
+      {checkFalsyData() ? (
+        <Flex direction="column" w="auto" m="4" mx="5">
+          <Box w="100%">
+            <VStack align="stretch" spacing={[2, 2, 2, 5, 5]}>
+              {title && (
                 <Flex w="100%" h="100%">
-                  <Box>Total Votes</Box>
-                  <Spacer />
-                  <Box>{votes.total_votes ? votes.total_votes : 0}</Box>
-                </Flex>
-                <Flex w="100%" h="100%">
-                  <Box>First Vote</Box>
+                  <Box>Title</Box>
                   <Spacer />
                   <Box>
-                    <Text as="kbd">{parseUTC(votes.first_vote_cast_at)}</Text>
+                    <Text>{title ? title : "N/A"}</Text>
                   </Box>
                 </Flex>
+              )}
+              {total_vp > 0 && (
                 <Flex w="100%" h="100%">
-                  <Box>Latest Vote</Box>
+                  <Box>Total VP</Box>
+                  <Spacer />
+                  <Box>{total_vp ? Math.round(total_vp) : "N/A"}</Box>
+                </Flex>
+              )}
+              {delegated_vp && (
+                <Flex w="100%" h="100%">
+                  <Box>Delegated VP</Box>
+                  <Spacer />
+                  <Box>{delegated_vp ? delegated_vp : "N/A"}</Box>
+                </Flex>
+              )}
+              {votes && (
+                <>
+                  <Flex w="100%" h="100%">
+                    <Box>Total Votes</Box>
+                    <Spacer />
+                    <Box>{votes.total_votes ? votes.total_votes : 0}</Box>
+                  </Flex>
+                  <Flex w="100%" h="100%">
+                    <Box>First Vote</Box>
+                    <Spacer />
+                    <Box>
+                      <Text as="kbd">{parseUTC(votes.first_vote_cast_at)}</Text>
+                    </Box>
+                  </Flex>
+                  <Flex w="100%" h="100%">
+                    <Box>Latest Vote</Box>
+                    <Spacer />
+                    <Box>
+                      <Text as="kbd">
+                        {parseUTC(votes.latest_vote_cast_at)}
+                      </Text>
+                    </Box>
+                  </Flex>
+                </>
+              )}
+              {active_dao_committee_member && (
+                <Flex w="100%" h="100%">
+                  <Box>Active DAO Committee Member</Box>
                   <Spacer />
                   <Box>
-                    <Text as="kbd">{parseUTC(votes.latest_vote_cast_at)}</Text>
+                    <Text
+                      color={active_dao_committee_member ? "green" : "gray"}
+                    >
+                      {active_dao_committee_member ? "Yes" : "No"}
+                    </Text>
                   </Box>
                 </Flex>
-              </>
-            )}
-            {active_dao_committee_member && (
-              <Flex w="100%" h="100%">
-                <Box>Active DAO Committee Member</Box>
-                <Spacer />
-                <Box>
-                  <Text color={active_dao_committee_member ? "green" : "gray"}>
-                    {active_dao_committee_member ? "Yes" : "No"}
-                  </Text>
-                </Box>
-              </Flex>
-            )}
-            {collection_creator && (
-              <Flex w="100%" h="100%">
-                <Box>Collection Creator</Box>
-                <Spacer />
-                <Box>
-                  <Text color={collection_creator ? "green" : "gray"}>
-                    {collection_creator ? "Yes" : "No"}
-                  </Text>
-                </Box>
-              </Flex>
-            )}
-            {collections && collections.length > 0 && (
-              <UserDAOActivityCollection
+              )}
+              {collection_creator && (
+                <Flex w="100%" h="100%">
+                  <Box>Collection Creator</Box>
+                  <Spacer />
+                  <Box>
+                    <Text color={collection_creator ? "green" : "gray"}>
+                      {collection_creator ? "Yes" : "No"}
+                    </Text>
+                  </Box>
+                </Flex>
+              )}
+              {collections && collections.length > 0 && (
+                <UserDAOActivityCollection
+                  name={name}
+                  collections={collections}
+                />
+              )}
+              <UserDAOAvtivityDelegate
                 name={name}
-                collections={collections}
+                delegate={delegate}
+                delegators={delegators}
               />
-            )}
-            <UserDAOAvtivityDelegate
-              name={name}
-              delegate={delegate}
-              delegators={delegators}
-            />
-            <UserDAOActivityGrant name={name} grants={grants} />
-            {teams && teams.length > 0 && (
-              <UserDAOActivityTeam name={name} teams={teams} />
-            )}
-            {proposals && proposals.count > 0 && (
-              <UserDAOActivityProposal name={name} proposals={proposals} />
-            )}
-          </VStack>
-        </Box>
-      </Flex>
+              <UserDAOActivityGrant name={name} grants={grants} />
+              {teams && teams.length > 0 && (
+                <UserDAOActivityTeam name={name} teams={teams} />
+              )}
+              {proposals && proposals.count > 0 && (
+                <UserDAOActivityProposal name={name} proposals={proposals} />
+              )}
+            </VStack>
+          </Box>
+        </Flex>
+      ) : (
+        <Center h="225px">
+          <Text color={useColorModeValue("gray.200", "gray.600")}>No Data</Text>
+        </Center>
+      )}
     </BoxWrapper>
   )
 }
--- src/components/local/stats/user/UserNFT.tsx
+++ src/components/local/stats/user/UserNFT.tsx
@@ -1,8 +1,16 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import BoxWrapper from "../../../layout/local/BoxWrapper"
-import { Box, Flex, Spacer, VStack, Text, Button } from "@chakra-ui/react"
-import CountUp from "react-countup"
+import {
+  useColorModeValue,
+  Box,
+  Flex,
+  Spacer,
+  VStack,
+  Text,
+  Button,
+  Center,
+} from "@chakra-ui/react"
 import BoxTitle from "../../../layout/local/BoxTitle"
-import moment from "moment"
 import { parseUTC } from "../../../../lib/hooks/utils"
 import Link from "next/link"
 
@@ -23,6 +31,29 @@ const UserNFT = ({ data, address }) => {
     first_wearable_acquired_at,
   } = data
 
+  const checkFalsyData = () => {
+    if (
+      !og_user &&
+      !owns_dclens &&
+      !owns_land &&
+      !owns_nfts &&
+      !owns_wearables &&
+      !participant_in_genesis_auction &&
+      !total_dclens &&
+      !total_lands &&
+      !total_wearables &&
+      !first_dclens_acquired_at &&
+      !first_land_acquired_at &&
+      !first_wearable_acquired_at
+    ) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  console.log(checkFalsyData())
+
   return (
     <BoxWrapper colSpan={[1, 1, 1, 2, 2]}>
       <BoxTitle
@@ -35,156 +66,162 @@ const UserNFT = ({ data, address }) => {
         line={false}
         setLine={{}}
       />
-      <Flex direction="column" w="auto" m="4" mx="5">
-        <Box w="100%">
-          <VStack align="stretch" spacing={[2, 2, 2, 5, 5]}>
-            {og_user && (
-              <Flex w="100%" h="100%">
-                <Box>OG User</Box>
-                <Spacer />
-                <Box>
-                  <Text color={og_user ? "green" : "gray"}>
-                    {og_user ? "Yes" : "No"}
-                  </Text>
-                </Box>
-              </Flex>
-            )}
-            {owns_nfts && (
-              <Flex w="100%" h="100%">
-                <Box>Owns NFT</Box>
-                <Spacer />
-                <Box>
-                  <Text>{owns_nfts && "Yes"}</Text>
-                </Box>
-              </Flex>
-            )}
-            {owns_dclens && (
-              <Flex w="100%" h="100%">
-                <Box>Owns DCLENS</Box>
-                <Spacer />
-                <Box>
-                  <Link
-                    href={`https://market.decentraland.org/accounts/${address}?assetType=nft&section=ens`}
-                    target="_blank"
-                  >
-                    <Button borderRadius="xl" size="xs">
-                      <Text fontWeight="bold">{owns_dclens && "Name"}</Text>
-                    </Button>
-                  </Link>
-                </Box>
-              </Flex>
-            )}
-            {owns_land && (
-              <Flex w="100%" h="100%">
-                <Box>Owns Land</Box>
-                <Spacer />
-                <Box>
-                  <Link
-                    href={`https://market.decentraland.org/accounts/${address}?assetType=nft&section=land`}
-                    target="_blank"
-                  >
-                    <Button borderRadius="xl" size="xs">
-                      <Text fontWeight="bold">{owns_land && "Land"}</Text>
-                    </Button>
-                  </Link>
-                </Box>
-              </Flex>
-            )}
+      {checkFalsyData() ? (
+        <Flex direction="column" w="auto" m="4" mx="5">
+          <Box w="100%">
+            <VStack align="stretch" spacing={[2, 2, 2, 5, 5]}>
+              {og_user && (
+                <Flex w="100%" h="100%">
+                  <Box>OG User</Box>
+                  <Spacer />
+                  <Box>
+                    <Text color={og_user ? "green" : "gray"}>
+                      {og_user ? "Yes" : "No"}
+                    </Text>
+                  </Box>
+                </Flex>
+              )}
+              {owns_nfts && (
+                <Flex w="100%" h="100%">
+                  <Box>Owns NFT</Box>
+                  <Spacer />
+                  <Box>
+                    <Text>{owns_nfts && "Yes"}</Text>
+                  </Box>
+                </Flex>
+              )}
+              {owns_dclens && (
+                <Flex w="100%" h="100%">
+                  <Box>Owns DCLENS</Box>
+                  <Spacer />
+                  <Box>
+                    <Link
+                      href={`https://market.decentraland.org/accounts/${address}?assetType=nft&section=ens`}
+                      target="_blank"
+                    >
+                      <Button borderRadius="xl" size="xs">
+                        <Text fontWeight="bold">{owns_dclens && "Name"}</Text>
+                      </Button>
+                    </Link>
+                  </Box>
+                </Flex>
+              )}
+              {owns_land && (
+                <Flex w="100%" h="100%">
+                  <Box>Owns Land</Box>
+                  <Spacer />
+                  <Box>
+                    <Link
+                      href={`https://market.decentraland.org/accounts/${address}?assetType=nft&section=land`}
+                      target="_blank"
+                    >
+                      <Button borderRadius="xl" size="xs">
+                        <Text fontWeight="bold">{owns_land && "Land"}</Text>
+                      </Button>
+                    </Link>
+                  </Box>
+                </Flex>
+              )}
 
-            {owns_wearables && (
-              <Flex w="100%" h="100%">
-                <Box>Owns Wearable</Box>
-                <Spacer />
-                <Box>
-                  <Link
-                    href={`https://market.decentraland.org/accounts/${address}?assetType=nft&section=wearable`}
-                    target="_blank"
-                  >
-                    <Button borderRadius="xl" size="xs">
-                      <Text fontWeight="bold">
-                        {owns_wearables && "Wearable"}
-                      </Text>
-                    </Button>
-                  </Link>
-                </Box>
-              </Flex>
-            )}
-            {participant_in_genesis_auction && (
-              <Flex w="100%" h="100%">
-                <Box>Participants In Genesis Auciton</Box>
-                <Spacer />
-                <Box>
-                  <Text
-                    color={participant_in_genesis_auction ? "green" : "gray"}
-                  >
-                    {participant_in_genesis_auction ? "Yes" : "No"}
-                  </Text>
-                </Box>
-              </Flex>
-            )}
-            {total_dclens > 0 && (
-              <Flex w="100%" h="100%">
-                <Box>Total DCLENS</Box>
-                <Spacer />
-                <Box>{total_dclens ? total_dclens : "N/A"}</Box>
-              </Flex>
-            )}
-            {total_lands !== 0 && (
-              <Flex w="100%" h="100%">
-                <Box>Total Lands</Box>
-                <Spacer />
-                <Box>{total_lands}</Box>
-              </Flex>
-            )}
-            {total_wearables && (
-              <Flex w="100%" h="100%">
-                <Box>Total Wearables</Box>
-                <Spacer />
-                <Box>{total_wearables > 999 ? "1000+" : total_wearables}</Box>
-              </Flex>
-            )}
-            {first_dclens_acquired_at && (
-              <Flex w="100%" h="100%">
-                <Box>First DCLENS</Box>
-                <Spacer />
-                <Box>
-                  <Text as="kbd" align="right" noOfLines={1}>
-                    {first_dclens_acquired_at
-                      ? parseUTC(first_dclens_acquired_at)
-                      : "N/A"}
-                  </Text>
-                </Box>
-              </Flex>
-            )}
-            {first_land_acquired_at && (
-              <Flex w="100%" h="100%">
-                <Box>First Land</Box>
-                <Spacer />
-                <Box>
-                  <Text as="kbd" align="right" noOfLines={1}>
-                    {first_land_acquired_at
-                      ? parseUTC(first_land_acquired_at)
-                      : "N/A"}
-                  </Text>
-                </Box>
-              </Flex>
-            )}
-            {first_wearable_acquired_at && (
-              <Flex w="100%" h="100%">
-                <Box>First Wearable</Box>
-                <Spacer />
-                <Box>
-                  <Text as="kbd" align="right" noOfLines={1}>
-                    {first_wearable_acquired_at
-                      ? parseUTC(first_wearable_acquired_at)
-                      : "N/A"}
-                  </Text>
-                </Box>
-              </Flex>
-            )}
-          </VStack>
-        </Box>
-      </Flex>
+              {owns_wearables && (
+                <Flex w="100%" h="100%">
+                  <Box>Owns Wearable</Box>
+                  <Spacer />
+                  <Box>
+                    <Link
+                      href={`https://market.decentraland.org/accounts/${address}?assetType=nft&section=wearable`}
+                      target="_blank"
+                    >
+                      <Button borderRadius="xl" size="xs">
+                        <Text fontWeight="bold">
+                          {owns_wearables && "Wearable"}
+                        </Text>
+                      </Button>
+                    </Link>
+                  </Box>
+                </Flex>
+              )}
+              {participant_in_genesis_auction && (
+                <Flex w="100%" h="100%">
+                  <Box>Participants In Genesis Auciton</Box>
+                  <Spacer />
+                  <Box>
+                    <Text
+                      color={participant_in_genesis_auction ? "green" : "gray"}
+                    >
+                      {participant_in_genesis_auction ? "Yes" : "No"}
+                    </Text>
+                  </Box>
+                </Flex>
+              )}
+              {total_dclens > 0 && (
+                <Flex w="100%" h="100%">
+                  <Box>Total DCLENS</Box>
+                  <Spacer />
+                  <Box>{total_dclens ? total_dclens : "N/A"}</Box>
+                </Flex>
+              )}
+              {total_lands && (
+                <Flex w="100%" h="100%">
+                  <Box>Total Lands</Box>
+                  <Spacer />
+                  <Box>{total_lands}</Box>
+                </Flex>
+              )}
+              {total_wearables && (
+                <Flex w="100%" h="100%">
+                  <Box>Total Wearables</Box>
+                  <Spacer />
+                  <Box>{total_wearables > 999 ? "1000+" : total_wearables}</Box>
+                </Flex>
+              )}
+              {first_dclens_acquired_at && (
+                <Flex w="100%" h="100%">
+                  <Box>First DCLENS</Box>
+                  <Spacer />
+                  <Box>
+                    <Text as="kbd" align="right" noOfLines={1}>
+                      {first_dclens_acquired_at
+                        ? parseUTC(first_dclens_acquired_at)
+                        : "N/A"}
+                    </Text>
+                  </Box>
+                </Flex>
+              )}
+              {first_land_acquired_at && (
+                <Flex w="100%" h="100%">
+                  <Box>First Land</Box>
+                  <Spacer />
+                  <Box>
+                    <Text as="kbd" align="right" noOfLines={1}>
+                      {first_land_acquired_at
+                        ? parseUTC(first_land_acquired_at)
+                        : "N/A"}
+                    </Text>
+                  </Box>
+                </Flex>
+              )}
+              {first_wearable_acquired_at && (
+                <Flex w="100%" h="100%">
+                  <Box>First Wearable</Box>
+                  <Spacer />
+                  <Box>
+                    <Text as="kbd" align="right" noOfLines={1}>
+                      {first_wearable_acquired_at
+                        ? parseUTC(first_wearable_acquired_at)
+                        : "N/A"}
+                    </Text>
+                  </Box>
+                </Flex>
+              )}
+            </VStack>
+          </Box>
+        </Flex>
+      ) : (
+        <Center h="225px">
+          <Text color={useColorModeValue("gray.200", "gray.600")}>No Data</Text>
+        </Center>
+      )}
     </BoxWrapper>
   )
 }
