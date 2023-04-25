46a8246cf706435e1cd7a6483faa536788c1cf31
Jongwoo Park <jongwoo.park@code.berlin>
2023-04-25 16:47:52 +0200

    loading state fixed on frequently visited scenes component

--- src/components/local/stats/user/UserTopScenes.tsx
+++ src/components/local/stats/user/UserTopScenes.tsx
@@ -52,173 +52,156 @@ const UserTopScenes = ({ address, userAddressRes }) => {
 
   const UserSceneTable = () => {
     return (
-      <Flex direction={["column", "column", "column", "column", "row"]}>
-        <Box
-          w={["auto", "auto", "auto", "auto", "50%"]}
-          mb="2"
-          mx="4"
-          fontSize="sm"
-        >
-          {data.slice(0, 10).map((item, i) => {
-            return (
-              <Box key={item.scene_uuid}>
-                <ToolTip label={`Open ${item.scene_name} page`}>
-                  <Link
-                    href={`/scenes/${mutateStringToURL(item.scene_name)}/${
-                      item.scene_uuid
-                    }`}
-                    target="_blank"
-                  >
-                    <Flex
-                      direction="row"
-                      px="4"
-                      py="2"
-                      borderRadius="xl"
-                      _hover={{
-                        bg: useColorModeValue("gray.100", "gray.600"),
-                        shadow: "md",
-                      }}
-                    >
-                      <Center mr="4">{i + 1}.</Center>
-                      <Flex direction={["column", "row"]}>
-                        <Center
-                          overflow="hidden"
-                          w={["150px", "200px", "200px", "200px", "250px"]}
-                          h="100px"
-                          border="2px solid"
-                          borderColor={useColorModeValue(
-                            "gray.200",
-                            "gray.600"
-                          )}
+      <>
+        {data.length > 0 ? (
+          <Flex direction={["column", "column", "column", "column", "row"]}>
+            <Box
+              w={["auto", "auto", "auto", "auto", "50%"]}
+              mb="2"
+              mx="4"
+              fontSize="sm"
+            >
+              {data.slice(0, 10).map((item, i) => {
+                return (
+                  <Box key={item.scene_uuid}>
+                    <ToolTip label={`Open ${item.scene_name} page`}>
+                      <Link
+                        href={`/scenes/${mutateStringToURL(item.scene_name)}/${
+                          item.scene_uuid
+                        }`}
+                        target="_blank"
+                      >
+                        <Flex
+                          direction="row"
+                          px="4"
+                          py="2"
                           borderRadius="xl"
-                          shadow="md"
+                          _hover={{
+                            bg: useColorModeValue("gray.100", "gray.600"),
+                            shadow: "md",
+                          }}
                         >
-                          <Image
-                            borderRadius="xl"
-                            shadow="md"
-                            objectFit="cover"
-                            alt={item.scene_name}
-                            src={item.map_url}
-                          />
-                        </Center>
-                        <Center ml="6">
-                          <Text fontSize={["xs", "sm"]} noOfLines={1}>
-                            {item.scene_name}
-                          </Text>
-                        </Center>
-                      </Flex>
-                      <Spacer />
-                      <Center>
-                        <Text
-                          as="kbd"
-                          fontSize={["xs", "sm"]}
-                          fontWeight="bold"
-                        >
-                          {convertSeconds(item.duration)}
-                        </Text>
-                      </Center>
-                    </Flex>
-                  </Link>
-                </ToolTip>
-                <Divider />
-              </Box>
-            )
-          })}
-        </Box>
+                          <Center mr="4">{i + 1}.</Center>
+                          <Flex direction={["column", "row"]}>
+                            <Center
+                              overflow="hidden"
+                              w={["150px", "200px", "200px", "200px", "250px"]}
+                              h="100px"
+                              border="2px solid"
+                              borderColor={useColorModeValue(
+                                "gray.200",
+                                "gray.600"
+                              )}
+                              borderRadius="xl"
+                              shadow="md"
+                            >
+                              <Image
+                                borderRadius="xl"
+                                shadow="md"
+                                objectFit="cover"
+                                alt={item.scene_name}
+                                src={item.map_url}
+                              />
+                            </Center>
+                            <Center ml="6">
+                              <Text fontSize={["xs", "sm"]} noOfLines={1}>
+                                {item.scene_name}
+                              </Text>
+                            </Center>
+                          </Flex>
+                          <Spacer />
+                          <Center>
+                            <Text
+                              as="kbd"
+                              fontSize={["xs", "sm"]}
+                              fontWeight="bold"
+                            >
+                              {convertSeconds(item.duration)}
+                            </Text>
+                          </Center>
+                        </Flex>
+                      </Link>
+                    </ToolTip>
+                    <Divider />
+                  </Box>
+                )
+              })}
+            </Box>
 
-        <Box
-          w={["auto", "auto", "auto", "auto", "50%"]}
-          mb="2"
-          mx="4"
-          fontSize="sm"
-        >
-          {data.slice(10, data.length).map((item, i) => {
-            return (
-              <Box key={item.scene_uuid}>
-                <ToolTip label={`Open ${item.scene_name} page`}>
-                  <Link
-                    href={`/scenes/${mutateStringToURL(item.scene_name)}/${
-                      item.scene_uuid
-                    }`}
-                    target="_blank"
-                  >
-                    <Flex
-                      direction="row"
-                      px="4"
-                      py="2"
-                      borderRadius="xl"
-                      _hover={{
-                        bg: useColorModeValue("gray.100", "gray.600"),
-                        shadow: "md",
-                      }}
-                    >
-                      <Center mr="4">{i + 11}.</Center>
-                      <Flex direction={["column", "row"]}>
-                        <Center
-                          overflow="hidden"
-                          w={["150px", "200px", "200px", "200px", "250px"]}
-                          h="100px"
-                          border="2px solid"
-                          borderColor={useColorModeValue(
-                            "gray.200",
-                            "gray.600"
-                          )}
+            <Box
+              w={["auto", "auto", "auto", "auto", "50%"]}
+              mb="2"
+              mx="4"
+              fontSize="sm"
+            >
+              {data.slice(10, data.length).map((item, i) => {
+                return (
+                  <Box key={item.scene_uuid}>
+                    <ToolTip label={`Open ${item.scene_name} page`}>
+                      <Link
+                        href={`/scenes/${mutateStringToURL(item.scene_name)}/${
+                          item.scene_uuid
+                        }`}
+                        target="_blank"
+                      >
+                        <Flex
+                          direction="row"
+                          px="4"
+                          py="2"
                           borderRadius="xl"
-                          shadow="md"
+                          _hover={{
+                            bg: useColorModeValue("gray.100", "gray.600"),
+                            shadow: "md",
+                          }}
                         >
-                          <Image
-                            borderRadius="xl"
-                            shadow="md"
-                            objectFit="cover"
-                            alt={item.scene_name}
-                            src={item.map_url}
-                          />
-                        </Center>
-                        <Center ml="6">
-                          <Text fontSize={["xs", "sm"]} noOfLines={1}>
-                            {item.scene_name}
-                          </Text>
-                        </Center>
-                      </Flex>
-                      <Spacer />
-                      <Center>
-                        <Text
-                          as="kbd"
-                          fontSize={["xs", "sm"]}
-                          fontWeight="bold"
-                        >
-                          {convertSeconds(item.duration)}
-                        </Text>
-                      </Center>
-                    </Flex>
-                  </Link>
-                </ToolTip>
+                          <Center mr="4">{i + 11}.</Center>
+                          <Flex direction={["column", "row"]}>
+                            <Center
+                              overflow="hidden"
+                              w={["150px", "200px", "200px", "200px", "250px"]}
+                              h="100px"
+                              border="2px solid"
+                              borderColor={useColorModeValue(
+                                "gray.200",
+                                "gray.600"
+                              )}
+                              borderRadius="xl"
+                              shadow="md"
+                            >
+                              <Image
+                                borderRadius="xl"
+                                shadow="md"
+                                objectFit="cover"
+                                alt={item.scene_name}
+                                src={item.map_url}
+                              />
+                            </Center>
+                            <Center ml="6">
+                              <Text fontSize={["xs", "sm"]} noOfLines={1}>
+                                {item.scene_name}
+                              </Text>
+                            </Center>
+                          </Flex>
+                          <Spacer />
+                          <Center>
+                            <Text
+                              as="kbd"
+                              fontSize={["xs", "sm"]}
+                              fontWeight="bold"
+                            >
+                              {convertSeconds(item.duration)}
+                            </Text>
+                          </Center>
+                        </Flex>
+                      </Link>
+                    </ToolTip>
 
-                <Divider />
-              </Box>
-            )
-          })}
-        </Box>
-      </Flex>
-    )
-  }
-
-  return (
-    <BoxWrapper colSpan={6}>
-      <BoxTitle
-        name={`Frequently Visited Scenes`}
-        description={`Top ${data.length} list of scenes ${userAddressRes.name} visited the most`}
-        date=""
-        avgData={""}
-        slicedData={[]}
-        color={""}
-        line={false}
-        setLine={{}}
-      />
-
-      {!isLoading ? (
-        data.length > 0 ? (
-          <UserSceneTable />
+                    <Divider />
+                  </Box>
+                )
+              })}
+            </Box>
+          </Flex>
         ) : (
           <Center h={chartProps.height}>
             <Text
@@ -235,11 +218,29 @@ const UserTopScenes = ({ address, userAddressRes }) => {
               No Data
             </Text>
           </Center>
-        )
-      ) : (
+        )}
+      </>
+    )
+  }
+
+  return (
+    <BoxWrapper colSpan={6}>
+      <BoxTitle
+        name={`Frequently Visited Scenes`}
+        description={`Top ${data.length} list of scenes ${userAddressRes.name} visited the most`}
+        date=""
+        avgData={""}
+        slicedData={[]}
+        color={""}
+        line={false}
+        setLine={{}}
+      />
+      {isLoading ? (
         <Center h={chartProps.height}>
           <Spinner />
         </Center>
+      ) : (
+        <UserSceneTable />
       )}
     </BoxWrapper>
   )
