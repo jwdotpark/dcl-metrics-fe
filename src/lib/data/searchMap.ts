b2d89636abd28f13407cc91a0c60d90e207ff63e
Jongwoo Park <jongwoo.park@code.berlin>
2023-05-16 00:53:13 +0200

    tile search fn unit test added

--- src/lib/data/searchMap.ts
+++ src/lib/data/searchMap.ts
@@ -1,11 +1,19 @@
-// @ts-nocheck
-export const searchTiles = (tiles: {}, keyword: string) => {
+interface Tile {
+  scene: {
+    name: string
+  }
+}
+
+export const searchTiles = (
+  tiles: Record<string, Tile>,
+  keyword: string
+): Tile[] => {
   if (!keyword) {
     return []
   }
 
-  let searchResult = []
-  const seenNames = new Set()
+  const searchResult: Tile[] = []
+  const seenNames = new Set<string>()
 
   Object.values(tiles).filter((tile) => {
     if (tile.scene) {
