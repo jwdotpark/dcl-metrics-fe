a0db17f17c8467be098370c5682dc2f2f0619ea0
Jongwoo Park <jongwoo.park@code.berlin>
2023-05-16 16:49:53 +0200

    cypress config fixed

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      if (config.isTextTerminal) {
+        return {
+          excludeSpecPattern: ["cypress/e2e/all.spec.cy.js"],
+        }
+      }
+    },
     chromeWebSecurity: false,
-    viewportHeight: 800,
-    viewportWidth: 800,
+    viewportWidth: 1280,
+    viewportHeight: 720,
     baseUrl: "http://localhost:3000",
     video: false,
     screenshotOnRunFailure: false,
