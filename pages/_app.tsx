ec67cfeeb95240e99a00f82e6e447864cbedc438
Jongwoo Park <jongwoo.park@code.berlin>
2023-06-06 00:10:35 +0200

    default config added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import "../styles/globals.css"
-import Head from "next/head"
 import Script from "next/script"
 import { theme } from "../src/lib/theme/theme"
 import type { AppProps } from "next/app"
@@ -7,6 +6,8 @@ import { ChakraProvider } from "@chakra-ui/react"
 import { Provider } from "jotai"
 import { Inter } from "@next/font/google"
 import { AnimatePresence } from "framer-motion"
+import { DefaultSeo } from "next-seo"
+import { generateMetaData, siteUrl } from "../src/lib/data/metadata"
 //import { generateMetaData } from "../src/lib/data/metadata"
 
 const InterFont = Inter({
@@ -23,17 +24,16 @@ function MyApp({
     return process.env.NEXT_PUBLIC_TELEMETRY === "true"
   }
 
-  //const pageTitle = "DCL-Metrics"
-  //const description =
-  //  "We make Decentraland's data accessible so it can be used by the community to build a better metaverse."
-  //const image = "/images/index.png"
+  const pageTitle = "DCL-Metrics"
+  const description =
+    "We make Decentraland's data accessible so it can be used by the community to build a better metaverse."
+  const image = `${siteUrl}/images/image.png`
 
-  //const metaData = generateMetaData({
-  //  title: pageTitle,
-  //  description: description,
-  //  image: image,
-  //})
-
+  const metaData = generateMetaData({
+    title: pageTitle,
+    description: description,
+    image: image,
+  })
   return (
     <ChakraProvider theme={theme}>
       <Provider>
@@ -64,6 +64,23 @@ function MyApp({
         {/* @ts-ignore */}
         <AnimatePresence initial={false} mode="wait">
           <main className={InterFont.className}>
+            <DefaultSeo
+              openGraph={{
+                url: siteUrl,
+                title: metaData.title,
+                description: metaData.description,
+                images: [
+                  {
+                    url: metaData.image,
+                    width: 400,
+                    height: 400,
+                    alt: metaData.description,
+                    type: "image/png",
+                  },
+                ],
+                siteName: "DCL-Metrics",
+              }}
+            />
             <Component {...pageProps} key={router.asPath} />
           </main>
         </AnimatePresence>
