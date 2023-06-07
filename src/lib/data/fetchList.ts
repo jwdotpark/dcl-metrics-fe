ef446e43fff75863dc8d0d148bb878786715c153
Jongwoo Park <jongwoo.park@code.berlin>
2023-06-07 11:51:14 +0200

    wip: fixing index fetch #2

--- src/lib/data/fetchList.ts
+++ src/lib/data/fetchList.ts
@@ -33,18 +33,18 @@ export const globalRequestList = [
     endpoint: "/dcl-property.rentals",
     staticData: staticLandSales,
   },
-  {
-    url: "https://services.itrmachines.com/val-analytics/topSellingLands?metaverse=decentraland",
-    endpint:
-      "https://services.itrmachines.com/val-analytics/topSellingLands?metaverse=decentraland",
-    staticData: staticTopLand,
-  },
-  {
-    url: "https://services.itrmachines.com/val-analytics/topPicks?metaverse=decentraland",
-    endpoint:
-      "https://services.itrmachines.com/val-analytics/topPicks?metaverse=decentraland",
-    staticData: staticTopPick,
-  },
+  //{
+  //  url: "https://services.itrmachines.com/val-analytics/topSellingLands?metaverse=decentraland",
+  //  endpint:
+  //    "https://services.itrmachines.com/val-analytics/topSellingLands?metaverse=decentraland",
+  //  staticData: staticTopLand,
+  //},
+  //{
+  //  url: "https://services.itrmachines.com/val-analytics/topPicks?metaverse=decentraland",
+  //  endpoint:
+  //    "https://services.itrmachines.com/val-analytics/topPicks?metaverse=decentraland",
+  //  staticData: staticTopPick,
+  //},
 ]
 
 export const globalFileNameArr = [
