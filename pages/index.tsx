584eabfba25c1f2b69e8dc63ece685166f438c0e
Jongwoo Park <jongwoo.park@code.berlin>
2023-10-30 18:04:32 +0100

    reverted world data component on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,7 +79,9 @@ const GlobalPage: NextPage = (props: Props) => {
       if (!isLocal) {
         const response = await fetch(`/api/fetch?url=${baseUrl + endpoint}`, {
           method: "GET",
-          headers: { "Content-Type": "application/json" },
+          headers: {
+            "Content-Type": "application/json",
+          },
         })
 
         const data = await response.json()
@@ -134,7 +136,7 @@ const GlobalPage: NextPage = (props: Props) => {
             <ActiveUsers />
           </Grid>
           <LandPicker parcelData={parcelRes} isPage={false} parcelCoord={{}} />
-          {/* {!isLoading && !error && Object.keys(worldData).length > 0 ? (
+          {!isLoading && !error && Object.keys(worldData).length > 0 ? (
             <Grid gap={4} templateColumns={`repeat(${gridColumn}, 1fr)`} mb="4">
               <WorldStat worldCurrentRes={worldData} isMainPage={true} />
               <WorldCurrentTop worldCurrentRes={worldData} pageSize={4} />
@@ -148,7 +150,7 @@ const GlobalPage: NextPage = (props: Props) => {
               </BoxWrapper>
               <Box mb="4" />
             </>
-          )} */}
+          )}
           <Box mb="4">
             <LandSales data={landSalesRes} />
           </Box>
