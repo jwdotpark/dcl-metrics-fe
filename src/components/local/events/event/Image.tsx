17010349d0b4028daac076127c8aa1024011ee43
Jongwoo Park <jongwoo.park@code.berlin>
2024-03-26 17:15:53 +0100

    image box layout is fixed

--- src/components/local/events/event/Image.tsx
+++ src/components/local/events/event/Image.tsx
@@ -4,22 +4,24 @@ import BoxWrapper from "../../../layout/local/BoxWrapper"
 export const ImageBox = ({ event }) => {
   return (
     <BoxWrapper colSpan={[8, 6]}>
-      <Box m="2" mb={[0, 4]}>
-        <Box
-          border="1px solid"
-          borderColor={useColorModeValue("gray.200", "gray.600")}
+      <Box
+        mt={[4, 2]}
+        mb={[0, 2]}
+        mx={[4, 2]}
+        border="1px solid"
+        borderColor={useColorModeValue("gray.200", "gray.600")}
+        borderRadius="xl"
+      >
+        <Image
+          w="100%"
+          h="100%"
+          maxH="500px"
           borderRadius="xl"
-        >
-          <Image
-            w="100%"
-            //maxH='500px'
-            borderRadius="xl"
-            shadow="md"
-            objectFit="cover"
-            alt={event.name}
-            src={event.image}
-          />
-        </Box>
+          shadow="md"
+          objectFit="cover"
+          alt={event.name}
+          src={event.image}
+        />
       </Box>
     </BoxWrapper>
   )
