5aa42d92dc42f7b04d31bc42bef145af13f7a181
Jongwoo Park <jongwoo.park@code.berlin>
2024-04-02 16:46:18 +0200

    wip: refactoring unique visitor chart

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,6 +31,8 @@ import { isLocal } from "../src/lib/data/constant"
 import staticWorldCurrent from "../public/data/staticWorldCurrent.json"
 import BoxWrapper from "../src/components/layout/local/BoxWrapper"
 
+import { Unique_Visitors } from "../src/components/local/stats/Unique_Visitors"
+
 export async function getStaticProps() {
   const globalData = await fetchGlobalData()
   //const rentalData = await fetchRentalData()
@@ -130,6 +132,8 @@ const GlobalPage: NextPage = (props: Props) => {
         <Box w="100%">
           <Box mb="4" data-testid="uniqueVisitors">
             <UniqueVisitors data={globalDailyRes} />
+            <Box mb="4" />
+            <Unique_Visitors data={globalDailyRes} />
           </Box>
           <Grid gap={4} templateColumns={`repeat(${gridColumn}, 1fr)`} mb="4">
             <UniqueVisitedParcels data={globalDailyRes} />
---
+++ src/components/layout/local/PlainBoxTitle.tsx
@@ -0,0 +1,36 @@
+import { useColorModeValue, Box, Text, Flex, Spacer } from "@chakra-ui/react"
+
+const BoxTitle = ({ name, description }) => {
+  return (
+    <Box w="100%">
+      <Flex direction={["column", "column", "row", "row"]} w="100%">
+        <Box mt="4">
+          <Flex direction="column" ml="5">
+            <Box>
+              <Text fontSize="xl" fontWeight="bold">
+                {name}
+              </Text>
+            </Box>
+
+            <Box mr="4">
+              <Text color="gray.500" fontSize={["xs", "sm"]}>
+                {description}
+              </Text>
+            </Box>
+          </Flex>
+        </Box>
+        <Spacer />
+      </Flex>
+      <Box
+        w="auto"
+        mt="2"
+        mb="4"
+        mx="4"
+        borderBottom="1px solid"
+        borderBottomColor={useColorModeValue("gray.200", "gray.700")}
+      />
+    </Box>
+  )
+}
+
+export default BoxTitle
