0193d6ffc134226940d725771a73d45d191bc96b
Jongwoo Park <jongwoo.park@code.berlin>
2024-04-11 19:45:51 +0200

    disabled marathon user & user related component on /events/id

--- src/components/local/events/event/EventRelatedScene.tsx
+++ src/components/local/events/event/EventRelatedScene.tsx
@@ -170,9 +170,9 @@ export const EventRelatedEvent = ({ event, data, itemsPerPage = 1 }) => {
             </Box>
           </Flex>
           <Flex direction={["column", "row"]} mt="4">
-            <Box w={["100%", "60%"]} mr={[0, 4]} mb={[2, 0]}>
+            {/*<Box w={["100%", "60%"]} mr={[0, 4]} mb={[2, 0]}>
               <SceneMarathonUsers data={sceneData.marathon_users} />
-            </Box>
+            </Box>*/}
             <Box w={["100%", "40%"]}>
               <SceneBarChart
                 visitors_by_hour_histogram={
