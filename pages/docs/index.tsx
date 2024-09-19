0516fb41978d1dec2fc0a9343bccc31f51fe9bf8
Jongwoo Park <jongwoo.park@code.berlin>
2024-09-19 17:32:10 +0200

    clean up

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,28 +1,14 @@
-import ApiLayout from "../../src/components/docs/ApiLayout"
-import ApiExample from "../../src/components/local/api/ApiExample"
-import { getApiLists } from "../../markdown/helpers/post"
-import { useState } from "react"
-
-export const getStaticProps = async () => {
-  const apiList = getApiLists()
-
+export const getServerSideProps = async () => {
   return {
-    props: { apiList },
+    redirect: {
+      destination: "/docs/api/basics",
+      permanent: false,
+    },
   }
 }
 
-const API = ({ apiList }) => {
-  const [selectedItem, setSelectedItem] = useState(apiList[0])
-
-  return (
-    <ApiLayout
-      selectedItem={selectedItem}
-      setSelectedItem={setSelectedItem}
-      apiList={apiList}
-    >
-      <ApiExample selectedItem={selectedItem} />
-    </ApiLayout>
-  )
+const API = () => {
+  return <></>
 }
 
 export default API
--- src/components/local/api/ApiExample.tsx
+++ src/components/local/api/ApiExample.tsx
@@ -5,8 +5,6 @@ import ReactMarkdown from "react-markdown"
 import { theme } from "../../markdown/theme"
 
 const ApiExample = ({ example }) => {
-  console.log(example)
-
   return (
     <BoxWrapper colSpan={[6, 6, 6, 4, 4]}>
       <Box p="4">
