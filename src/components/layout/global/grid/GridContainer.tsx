89416ce9e4149e60273230fdf2155e321588f774
Jongwoo Park <jongwoo.park@code.berlin>
2024-11-20 13:05:19 +0100

    clean up

--- src/components/layout/global/grid/GridContainer.tsx
+++ src/components/layout/global/grid/GridContainer.tsx
@@ -21,8 +21,8 @@ export const GridContainer = ({ chartData }) => {
   const defaultLayout = [
     { i: "1", x: 0, y: 0, w: 1, h: 1, isResizable: false },
     { i: "2", x: 1, y: 0, w: 1, h: 1, isResizable: false },
-    { i: "3", x: 0, y: 1, w: 2, h: 1 },
-    { i: "4", x: 1, y: 1, w: 2, h: 1 },
+    //{ i: "3", x: 0, y: 1, w: 2, h: 1 },
+    //{ i: "4", x: 1, y: 1, w: 2, h: 1 },
   ]
 
   const [layout, setLayout] = useState(getSavedLayout() || defaultLayout)
--- src/components/layout/global/grid/GridItemContainer.tsx
+++ src/components/layout/global/grid/GridItemContainer.tsx
@@ -3,6 +3,7 @@ import { Box, useColorModeValue } from "@chakra-ui/react"
 export const GridItemContainer = ({ children }) => {
   return (
     <Box
+      h="350px"
       p="4"
       bg={useColorModeValue("gray.200", "gray.700")}
       border="1px solid"
