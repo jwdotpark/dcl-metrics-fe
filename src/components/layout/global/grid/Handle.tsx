aca6f66805caaed24ca1dedff7a1cb7b4879204d
Jongwoo Park <jongwoo.park@code.berlin>
2024-11-23 21:14:54 +0100

    clean up

--- src/components/layout/global/grid/Handle.tsx
+++ src/components/layout/global/grid/Handle.tsx
@@ -1,4 +1,4 @@
-import { IconButton } from "@chakra-ui/react"
+import { IconButton, useColorModeValue } from "@chakra-ui/react"
 import { FiMove } from "react-icons/fi"
 
 export const Handle = () => {
@@ -6,6 +6,9 @@ export const Handle = () => {
     <IconButton
       className="drag-handle"
       fontSize="20px"
+      border="1px"
+      borderColor={useColorModeValue("gray.300", "gray.600")}
+      shadow="md"
       aria-label="move button"
       icon={<FiMove size="13px" />}
       rounded="full"
