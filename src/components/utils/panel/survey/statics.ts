a2a4914ad656f8a58843b5661736e1089fe081e7
Jongwoo Park <jongwoo.park@code.berlin>
2025-01-23 16:18:32 +0100

    wip: classifiying questions

--- src/components/utils/panel/survey/statics.ts
+++ src/components/utils/panel/survey/statics.ts
@@ -1,35 +1,101 @@
 export const inquiries = [
   {
     question:
-      "What was the total number of active users in Decentraland in March 2024?",
-    options: ["10,000", "25,000", "50,000", "75,000"],
-    answer: "25,000",
+      "What was the total number of active users in Decentraland in November 2024?",
+    options: [
+      "8,000 - 9,000",
+      "9,000 - 10,000",
+      "10,000 - 11,000",
+      "15,000 - 16,000",
+    ],
+    answer: "9,000 - 10,000",
+  },
+  {
+    question: "What day had the most unique users in 2022?",
+    options: ["Jan 12", "Jul 17", "Nov 10", "Dec 29"],
+    answer: "Nov 10",
   },
   {
     question:
-      "In which month of 2024 was the highest number of land parcels traded?",
-    options: ["January", "March", "June", "September"],
-    answer: "June",
+      "Can you tell the entry point of the data collection for the number of activated global scene?",
+    options: ["2022 Mar 3", "2022 Jul 10", "2023 Apr 18", "2024 Aug 15"],
+    answer: "2022 Jul 10",
   },
   {
-    question: "What was the range of marketplace sales volume in 2024?",
-    options: [
-      "10,000 – 50,000 MANA",
-      "50,000 – 100,000 MANA",
-      "100,000 – 200,000 MANA",
-      "200,000 – 300,000 MANA",
-    ],
-    answer: "100,000 – 200,000 MANA",
+    question: "Who has visited the most parcel in last 7 days?",
+    options: ["mrz98", "Pibcw", "Heiko", "NaldoBenny"],
+    answer: "Pibcw",
   },
   {
     question:
-      "How did the average price of land parcels in Decentraland change over the second half of 2024?",
+      "In which parcel did people spend the second most time on average, over the last 90 days?",
+    options: ["[80, -21]", "[37, 103]", "[99, 87]", "[-28, 55]"],
+    answer: "[37,103]",
+  },
+  {
+    question:
+      "Can you tell the name of the scene that the said parcel is in now?",
     options: [
-      "Decreased by 10%",
-      "Stayed the same",
-      "Increased by 5%",
-      "Increased by 15%",
+      "The Lost City",
+      "Genesis Plaza",
+      "Soul Magic",
+      "Decentraland University",
     ],
-    answer: "Stayed the same",
+    answer: "The Lost City",
+  },
+  {
+    question: "Which entry point parcel gets the most users trying to log in?",
+    options: ["[0, 0]", "[0, 83]", "[61, -61]", "[-83, 0]"],
+    answer: "[0, 0]",
+  },
+  {
+    question: "In which scenes do users most often attempt to log out?",
+    options: [
+      "ICE Poker - The Stronghold",
+      "Tonomiya",
+      "Exodus",
+      "Golfcraft Minigolf",
+    ],
+    answer: "ICE Poker - The Stronghold",
+  },
+  {
+    question:
+      "Which parcel gets the most visitors, regardless of the time spent by the user in last 90 days?",
+    options: ["[75, 8]", "[106, 75]", "[37, 103]", "[-100, 127]"],
+    answer: "[-100, 127]",
+  },
+  {
+    question:
+      "Can you tell the date when DCL-Metrics has started to collect the ENS world?",
+    options: ["2022 Apr", "2023 Feb", "2023 Oct", "2024 Jan"],
+    answer: "2023 Oct",
+  },
+  {
+    question: "Can you tell how many worlds are occupied on 2023 Nov 20?",
+    options: ["4", "7", "9", "21"],
+    answer: "9",
+  },
+  {
+    question: "Can you tell who created the party that has the most attendees?",
+    options: ["VTATV", "AurelieLebon", "Cosy", "bitfiend"],
+    answer: "Cosy",
+  },
+  {
+    question:
+      "Can you tell if the users mentioned above are participating in DAO activities?",
+    options: ["Yes", "No"],
+    answer: "Yes",
+  },
+  {
+    question:
+      "What is the average time spent by users in the scene with the highest number of unique visitors yesterday?",
+    options: ["1m 30s - 2m", "3m - 3m 30s", "6m - 6m 30s", "10m 30s - 11m"],
+    answer: "6m - 6m 30s",
+  },
+  {
+    question:
+      "Which parcel has the highest number of Away From keyboard visitors in the last 30 days?",
+    options: ["[129, -124]", "[-102, -141]", "[-2, -108]", "[0, -1]"],
+    answer: "[-102, -141]",
   },
 ]
